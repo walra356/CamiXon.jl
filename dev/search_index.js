a379704855ab897db95e7035ed39fe1b2cb2d9f7
Documenter.jl <documenter@juliadocs.github.io>
2021-01-02 19:07:53 +0000

    build based on 2ef15ef

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CamiXon","category":"page"},{"location":"#CamiXon","page":"Home","title":"CamiXon","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CamiXon]","category":"page"}]
+}
