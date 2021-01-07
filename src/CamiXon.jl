ca568cab7d9338a5ccc7e8505564218dc29645df
Jook Walraven <walra356@planet.nl>
2021-01-07 17:26:52 +0100

    update CamiXon

--- src/CamiXon.jl
+++ src/CamiXon.jl
@@ -1,5 +1,139 @@
 module CamiXon
 
-# Write your package code here.
+export get_indices
+export get_indices_count
+export func
+
+"""
+    func(x)
+
+Returns double the number `x` plus `1`.
+"""
+func(x) = 2x + 1
+
+
+"""
+    p(n::Int,k::Int)
+    the number of partitions of n in k parts
+"""
+function p(n::Int,k::Int)
+    (n<0)|(k<0)|(k>n) ? 0 : (k==n)|(k==1) ? 1 : p(n-k,k) + p(n-1,k-1)
+end
+
+"""
+    p(n::Int,k::Int)
+    the total number of partitions of n
+"""
+function p(n) #total number of integer partions of n
+    c = 1
+    for k=2:n c += p(n,k) end
+    c
+end
+
+
+
+"""
+    get_indices(A::AbstractArray{T,N}, a::T...)
+
+Find the index (indices) of selected Array elements (default: all elements).
+
+#### Examples:
+```
+A = collect("ahsgh")
+get_indices(A,'h')
+1-element Array{Array{Int64,1},1}:
+ [2, 5]
+
+get_indices(A)
+4-element Array{Array{Int64,1},1}:
+ [1]
+ [2, 5]
+ [3]
+ [4]
+```
+"""
+function get_indices(A::AbstractArray{T,N}, a::T...)  where {T,N}
+    a == () ? a = unique(A) : false
+    [findall(A .== fill(a[i],length(A))) for i in eachindex(a)]
+end
+
+
+"""
+    get_indices_count(A::AbstractArray{T,N}, a::T...)  where {T,N}
+
+Count the number of indices of selected Array elements (default: all elements).
+
+#### Examples:
+```
+A = collect("ahsgh")
+get_indices_count(A,'h')
+1-element Array{Array{Int64,1},1}:
+ 2
+
+get_indices_count(A)
+4-element Array{Array{Int64,1},1}:
+ 1
+ 2
+ 1
+ 1
+```
+"""
+function get_indices_count(A::AbstractArray{T,N}, a::T...)  where {T,N}
+    a == () ? a = unique(A) : false
+    [length(findall(A .== fill(a[i],length(A)))) for i in eachindex(a)]
+end
+
+
+"""
+    find_count(A::AbstractArray{T,N}, a::T...)  where {T,N}
+
+Count the number of indices of selected Array elements (default: all elements).
+
+#### Examples:
+```
+A = collect("ahsgh")
+find_count(A,'h')
+1-element Array{Array{Int64,1},1}:
+ 2
+
+find_count(A)
+4-element Array{Array{Int64,1},1}:
+ 1
+ 2
+ 1
+ 1
+```
+"""
+function find_count(A::AbstractArray{T,N}, a::T...)  where {T,N}
+    a == () ? a = unique(A) : false
+    [length(findall(A .== fill(a[i],length(A)))) for i in eachindex(a)]
+end
+
+
+"""
+    permutation_count(A::AbstractArray{T,N}; unique = false)  where {T,N}
+
+Count the number of permutations (option: unique permutations).
+
+#### Examples:
+```
+A = collect("ahsgh")
+permutation_count(A)
+ 120
+
+permutation_count(A; unique=true)
+ 60
+```
+"""
+function permutation_count(A::AbstractArray{T,N}; unique = false)  where {T,N}
+    if unique
+        o = factorial(length(A))
+        c = find_count(A)
+        for i in eachindex(c) o = o ÷ c[i] end
+        return out
+    else
+        return factorial(length(A)) # default
+    end
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,8 @@ using CamiXon
 using Test
 
 @testset "CamiXon.jl" begin
-    # Write your tests here.
+    @test get_indices(collect("ahsgh")) == [[1], [2, 5], [3], [4]]
+    @test get_indices(collect("ahsgh"),'h') == [[2, 5]]
+    @test get_indices([1,2,3,4,2]) == [[1], [2, 5], [3], [4]]
+    @test get_indices([1,2,3,4,2],2) == [[2, 5]]
 end
