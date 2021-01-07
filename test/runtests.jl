9ccd0033e90f6ac5bba0b27513e9196bae379d57
Jook Walraven <69215586+walra356@users.noreply.github.com>
2021-01-07 17:39:59 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,2 @@
 using CamiXon
 using Test
-
-@testset "CamiXon.jl" begin
-    @test find_indices(collect("ahsgh")) == [[1], [2, 5], [3], [4]]
-    @test find_indices(collect("ahsgh"),'h') == [[2, 5]]
-    @test find_indices([1,2,3,4,2]) == [[1], [2, 5], [3], [4]]
-    @test find_indices([1,2,3,4,2],2) == [[2, 5]]
-end
