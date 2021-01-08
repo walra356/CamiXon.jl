002084adddd8eaed27076438e31fc746d8160db9
Jook Walraven <69215586+walra356@users.noreply.github.com>
2021-01-09 00:36:37 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ using Test
     @test indices(collect("ahsgh"),'h') == [[2, 5]]
     @test indices_cnt(collect("ahsgh")) == [1, 2, 1, 1]
     @test indices_cnt(collect("ahsgh"),'h') == [2]
-    @test permutations_cnt(collect("ahsgh")) == 120
-    @test permutations_cnt(collect("ahsgh");unique=true) == 60
+    #@test permutations_cnt(collect("ahsgh")) == 120
+    #@test permutations_cnt(collect("ahsgh");unique=true) == 60
 end
 
