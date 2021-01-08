35b97082cd36549318a0e8684375445e09cb5725
Jook Walraven <69215586+walra356@users.noreply.github.com>
2021-01-08 23:54:28 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,9 @@ using Test
 @testset "CamiXon.jl" begin
     @test indices(collect("ahsgh")) == [[1], [2, 5], [3], [4]]
     @test indices(collect("ahsgh"),'h') == [[2, 5]]
-    @test indices_cnt(collect("ahsgh")) == [1, 2, 1, 1]
-    @test indices_cnt(collect("ahsgh"),'h') == [2]
-    @test permutations_cnt(collect("ahsgh")) == 120
-    @test permutations_cnt(collect("ahsgh");unique=true) == 60
+    #@test indices_cnt(collect("ahsgh")) == [1, 2, 1, 1]
+    #@test indices_cnt(collect("ahsgh"),'h') == [2]
+    #@test permutations_cnt(collect("ahsgh")) == 120
+    #@test permutations_cnt(collect("ahsgh");unique=true) == 60
 end
 
