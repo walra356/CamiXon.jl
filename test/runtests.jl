248d1a5944ab06a90161bccb99f418275daac064
Jook Walraven <69215586+walra356@users.noreply.github.com>
2021-01-08 23:37:02 +0100

    Update runtests.jl
    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,12 @@
 using CamiXon
 using Test
 
-#@testset "CamiXon.jl" begin
-#    @test indices(collect("ahsgh")) == [[1], [2, 5], [3], [4]]
-#    @test indices(collect("ahsgh"),'h') == [[2, 5]]
-#    @test indices([1,2,3,4,2]) == [[1], [2, 5], [3], [4]]
-#    @test indices([1,2,3,4,2],2) == [[2, 5]]
+@testset "CamiXon.jl" begin
+    @test indices(collect("ahsgh")) == [[1], [2, 5], [3], [4]]
+    @test indices(collect("ahsgh"),'h') == [[2, 5]]
+    @test indices_cnt(collect("ahsgh")) == [1, 2, 1, 1]
+    @test indices_cnt(collect("ahsgh"),'h') == [2]
+    @test permutations_cnt(collect("ahsgh")) == 120
+    @test permutations_cnt(collect("ahsgh")) == 60
 #end
 
