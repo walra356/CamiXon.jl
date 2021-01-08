a84350ff302e73a6da92d4d013ebfc167d6e0832
Jook Walraven <69215586+walra356@users.noreply.github.com>
2021-01-08 22:11:49 +0100

    Revert "Update CamiXon.jl"

--- src/CamiXon.jl
+++ src/CamiXon.jl
@@ -1,45 +1,43 @@
 module CamiXon
 
-export indices
-export indices_cnt
-export partitions_cnt
-export indices_cnt
-export permutations_cnt
+export get_indices
+export get_indices_count
+export get_permutation_count
 
 
 """
-    partitions_cnt(n::Int,k::Int)
+    p(n::Int,k::Int)
     the number of partitions of n in k parts
 """
-function partitions_cnt(n::Int,k::Int)
-    (n<0)|(k<0)|(k>n) ? 0 : (k==n)|(k==1) ? 1 : partitions_cnt(n-k,k) + partitions_cnt(n-1,k-1)
+function p(n::Int,k::Int)
+    (n<0)|(k<0)|(k>n) ? 0 : (k==n)|(k==1) ? 1 : p(n-k,k) + p(n-1,k-1)
 end
 
 
 """
-    partitions_cnt(n::Int)
+    p(n::Int,k::Int)
     the total number of partitions of n
 """
-function partitions_cnt(n::Int) 
+function p(n) #total number of integer partions of n
     c = 1
-    for k=2:n c += partitions_cnt(n,k) end
+    for k=2:n c += p(n,k) end
     c
 end
 
 
 """
-    function indices(A::AbstractArray{T,N}, a::T...)  where {T,N}
+    function get_indices(A::AbstractArray{T,N}, a::T...)  where {T,N}
 
 Find the index (indices) of selected Array elements (default: all elements).
 
 #### Examples:
 ```
 A = collect("ahsgh")
-indices(A,'h')
+get_indices(A,'h')
 1-element Array{Array{Int64,1},1}:
  [2, 5]
 
-indices(A)
+get_indices(A)
 4-element Array{Array{Int64,1},1}:
  [1]
  [2, 5]
@@ -47,25 +45,25 @@ indices(A)
  [4]
 ```
 """
-function indices(A::AbstractArray{T,N}, a::T...)  where {T,N}
+function get_indices(A::AbstractArray{T,N}, a::T...)  where {T,N}
     a == () ? a = unique(A) : false
     [findall(A .== fill(a[i],length(A))) for i in eachindex(a)]
 end
 
 
 """
-    indices_cnt(A::AbstractArray{T,N}, a::T...)  where {T,N}
+    get_indices_count(A::AbstractArray{T,N}, a::T...)  where {T,N}
 
 Count the number of indices of selected Array elements (default: all elements).
 
 #### Examples:
 ```
 A = collect("ahsgh")
-indices_cnt(A,'h')
+get_indices_count(A,'h')
 1-element Array{Array{Int64,1},1}:
  2
 
-indices_cnt(A)
+get_indices_count(A)
 4-element Array{Array{Int64,1},1}:
  1
  2
@@ -73,28 +71,28 @@ indices_cnt(A)
  1
 ```
 """
-function indices_cnt(A::AbstractArray{T,N}, a::T...)  where {T,N}
+function get_indices_count(A::AbstractArray{T,N}, a::T...)  where {T,N}
     a == () ? a = unique(A) : false
     [length(findall(A .== fill(a[i],length(A)))) for i in eachindex(a)]
 end
 
 
 """
-    permutation_cnt(A::AbstractArray{T,N}; unique = false)  where {T,N}
+    get_permutation_count(A::AbstractArray{T,N}; unique = false)  where {T,N}
 
 Count the number of permutations (option: unique permutations).
 
 #### Examples:
 ```
 A = collect("ahsgh")
-permutation_cnt(A)
+permutation_count(A)
  120
 
-permutation_cnt(A; unique=true)
+permutation_count(A; unique=true)
  60
 ```
 """
-function permutation_cnt(A::AbstractArray{T,N}; unique = false)  where {T,N}
+function get_permutation_count(A::AbstractArray{T,N}; unique = false)  where {T,N}
     if unique
         o = factorial(length(A))
         c = find_count(A)
