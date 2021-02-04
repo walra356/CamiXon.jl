430b4d56564e90e53a36718b83503d61485bf36f
Jook Walraven <69215586+walra356@users.noreply.github.com>
2021-02-04 22:50:41 +0100

    Update runtests.jl
    permutations_count test removed

--- test/runtests.jl
+++ test/runtests.jl
@@ -29,6 +29,5 @@ using Test
     @test integer_partitions(7,4) == [[4, 3], [4, 2, 1], [4, 1, 1, 1]]
     @test integer_partitions(7,4;count=true) == 3
     @test integer_partitions(7; transpose=true) == [[7], [4, 3], [5, 2], [6, 1], [3, 2, 2], [3, 3, 1], [4, 2, 1], [5, 1, 1], [2, 2, 2, 1], [3, 2, 1, 1], [4, 1, 1, 1], [2, 2, 1, 1, 1], [3, 1, 1, 1, 1], [2, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1]]
-    @test permutations_cnt(collect("ahsgh")) == 120
-    @test permutations_cnt(collect("ahsgh");unique=true) == 60
+
 end
