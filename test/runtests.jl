470de824e1580666c2b14ecef8edb0d1ed2442e8
Jook Walraven <69215586+walra356@users.noreply.github.com>
2021-02-04 22:28:08 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,9 +20,9 @@ using Test
     @test find_last([:📑,:📌,:📢,:📌,:📞], :📌) == [4]
     @test find_last([:📑,:📌,:📢,:📌,:📞], :📌; dict=true) == [:📌 => 4]
     @test find_last([:📑,:📌,:📢,:📌,:📞]) == find_last([1,2,3,2,5]) == find_last("aβcβd")
-    @test canonical_partitions(6; header=true)==[[1, 1, 1, 1, 1, 1], [2, 2, 2], [3, 3], [4, 2], [5, 1], [6]]
-    @test canonical_partitions(6)==[[1, 1, 1, 1, 1, 1], [2, 2, 2], [3, 3], [4, 2], [5, 1], [6]]
-    @test canonical_partitions(6; header=true, reverse=false)==[[6], [5, 1], [4, 2], [3, 3], [2, 2, 2], [1, 1, 1, 1, 1, 1]]
+    @test canonical_partitions(6; header=true) == [[1, 1, 1, 1, 1, 1], [2, 2, 2], [3, 3], [4, 2], [5, 1], [6]]
+    @test canonical_partitions(6) == [[1, 1, 1, 1, 1, 1], [2, 2, 2], [3, 3], [4, 2], [5, 1], [6]]
+    @test canonical_partitions(6; header=true, reverse=false) == [[6], [5, 1], [4, 2], [3, 3], [2, 2, 2], [1, 1, 1, 1, 1, 1]]
     @test integer_partitions(7) == [[1, 1, 1, 1, 1, 1, 1], [2, 2, 2, 1], [2, 2, 1, 1, 1], [2, 1, 1, 1, 1, 1], [3, 3, 1], [3, 2, 2], [3, 2, 1, 1], [3, 1, 1, 1, 1], [4, 3], [4, 2, 1], [4, 1, 1, 1], [5, 2], [5, 1, 1], [6, 1], [7]]
     @test integer_partitions(9;count=true) == 30
     @test integer_partitions(9) == unique(integer_partitions(9))
