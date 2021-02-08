0cbcd651348fad9f1cf9cd3ec7f49428770cae75
Jook Walraven <69215586+walra356@users.noreply.github.com>
2021-02-08 20:56:48 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -31,7 +31,7 @@ using Test
     @test integer_partitions(7,4;count=true) == 3
     @test integer_partitions(7; transpose=true) == [[7], [4, 3], [5, 2], [6, 1], [3, 2, 2], [3, 3, 1], [4, 2, 1], [5, 1, 1], [2, 2, 2, 1], [3, 2, 1, 1], [4, 1, 1, 1], [2, 2, 1, 1, 1], [3, 1, 1, 1, 1], [2, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1]]
     @test decompose_filnam("T01.fits") == Dict("Extension" => ".FITS","Numerator" => "01","Prefix" => "T","Name" => "T01") 
-    @test fits_info("T01.fits") == T01.FITS: file was found (for more information set info=true)
+    @test fits_info("T01.fits") == "T01.FITS: file was found (for more information set info=true)"
     @test fits_info("T03.fits") == "Error: T03.FITS (file not found)"
     @test fits_copy("T03.fits") == "Error: T03.FITS (file not found)"
     @test fits_copy("T01.fits") == "T01.FITS was saved as T01 - Copy.FITS" 
