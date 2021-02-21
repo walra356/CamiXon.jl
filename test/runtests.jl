7c98a02efd49fae15e71c256b36d51df243a903e
Jook Walraven <69215586+walra356@users.noreply.github.com>
2021-02-21 14:01:07 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -30,9 +30,9 @@ using Test
     @test integer_partitions(7,4) == [[4, 3], [4, 2, 1], [4, 1, 1, 1]]
     @test integer_partitions(7,4;count=true) == 3
     @test integer_partitions(7; transpose=true) == [[7], [4, 3], [5, 2], [6, 1], [3, 2, 2], [3, 3, 1], [4, 2, 1], [5, 1, 1], [2, 2, 2, 1], [3, 2, 1, 1], [4, 1, 1, 1], [2, 2, 1, 1, 1], [3, 1, 1, 1, 1], [2, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1]]
-    @test decompose_filnam("T01.fits") == Dict("Extension" => ".FITS","Numerator" => "01","Prefix" => "T","Name" => "T01") 
-    @test fits_info("T01.fits") == "T01.FITS: file was found (for more information set info=true)"
-    @test fits_copy("T01.fits") == "T01.FITS was saved as T01 - Copy.FITS" 
-    @test fits_copy("T01.fits","T01a.fits") == "T01.FITS was saved as T01A.FITS"
+    @test decompose_filnam("T01.fits") == Dict("Extension" => ".fits","Numerator" => "01","Prefix" => "T","Name" => "T01") 
+    @test fits_info("T01.fits") == "T01.fits: file was found (for more information set info=true)"
+    @test fits_copy("T01.fits") == "T01.fits was saved as T01 - Copy.fits" 
+    @test fits_copy("T01.fits","T01a.fits") == "T01.fits was saved as T01a.fits"
 
 end
