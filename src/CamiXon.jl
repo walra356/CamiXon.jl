051c707e88a250c4abd5e670138245132137fb8f
Jook Walraven <69215586+walra356@users.noreply.github.com>
2021-04-15 21:43:18 +0200

    Update CamiXon.jl

--- src/CamiXon.jl
+++ src/CamiXon.jl
@@ -1,23 +1,13 @@
 module CamiXon
 
-export decompose_filnam
-export fits_combine
+
+export fits_create
+export fits_read
+export fits_extend
 export fits_copy
-export fits_info
-export fits_key_copy
-export fits_key_create
-export fits_key_delete
-export fits_key_edit
-export fits_key_info
-export fits_key_rename
-export find_all
-export find_first
-export find_last
-export canonical_partitions
-export integer_partitions
-export FORTRAN_format
-export cast_FORTRAN_format
-export cast_FORTRAN_datatype
+export fits_add_key
+export fits_edit_key
+export fits_delete_key
 export FITS_name
 export cast_FITS_name
 export FITS_table
@@ -25,8 +15,28 @@ export FITS_header
 export FITS_data
 export FITS_HDU
 export print_hduinfo
+export parse_FITS_TABLE
+export FORTRAN_format
+export cast_FORTRAN_format
+export cast_FORTRAN_datatype
 
-include("file_manipulation.jl")
+#export decompose_filnam
+#export fits_combine
+#export fits_copy
+#export fits_info
+#export fits_key_copy
+#export fits_key_create
+#export fits_key_delete
+#export fits_key_edit
+#export fits_key_info
+#export fits_key_rename
+export find_all
+export find_first
+export find_last
+export canonical_partitions
+export integer_partitions
+
+#include("file_manipulation.jl")
 include("fits_pointers.jl")
 include("read_io.jl")
 include("write_io.jl")
