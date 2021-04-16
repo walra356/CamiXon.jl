583abac8eba650971b4defb2f7ca2126a8ca2125
Jook Walraven <69215586+walra356@users.noreply.github.com>
2021-04-16 20:12:56 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -33,4 +33,8 @@ using Test
     #@test fits_info("T01.fits") == "T01.fits: file was found (for more information set info=true)"
     #@test fits_copy("T01.fits") == "T01.fits was saved as T01 - Copy.fits" 
     #@test fits_copy("T01.fits","T01a.fits";protect=false) == "T01.fits was saved as T01a.fits"
+    @test _run_fits_create()
+    @test _run_fits_extend()
+    @test _run_fits_read()
+    @test _run_fits_info()
 end
