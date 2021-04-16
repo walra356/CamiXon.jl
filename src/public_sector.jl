53b06afac40683bc2324bf517bcbb4fc73077804
Jook Walraven <69215586+walra356@users.noreply.github.com>
2021-04-16 22:05:29 +0200

    Update public_sector.jl

--- src/public_sector.jl
+++ src/public_sector.jl
@@ -72,7 +72,7 @@ println(a);println(b)
  ["SIMPLE", "BITPIX", "NAXIS", "NAXIS1", "BZERO", "BSCALE", "EXTEND", "COMMENT", "END"]
 
 @test fits_create()
- \\bf{passed test}
+ \\bf{Test Passed}
 ```
 """
 function fits_create(filename::String, data=[]; protect=true)
