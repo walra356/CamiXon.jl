ebc6438860662f4eda5977a7002381ab7c63aef4
= <walra356@planet.nl>
2021-04-30 17:18:14 +0200

    more on plotting

--- src/plotting2.jl
+++ src/plotting2.jl
@@ -1,3 +1,77 @@
+function show_scene(hdu::FITS_HDU, img=(0,0), mul=1.0; inline=false, res=(900,600), footnote::String=" ", textsize=10, xcenter=false, ycenter=false)
+
+    typeof(hdu) <: FITS_HDU || error("'$hdu': is not a FITS_HDU")
+
+    GLMakie.activate!()
+
+    AbstractPlotting.inline!(inline)
+
+    data = hdu.dataobject.data
+    dict = hdu.header.dict
+
+    valmin = minimum(data)
+    valmax = maximum(data)*mul
+
+    colormap = :gist_earth
+
+    nx = get(dict,"NAXIS1",0)
+    ny = get(dict,"NAXIS2",0)
+    nz = get(dict,"NAXIS3",0)
+    bp = get(dict,"BITPIX",0)
+    bz = get(dict,"BZERO",0)
+    nk = get(dict,"NUMKIN",0)
+
+    (yticks,xticks) = set_ticks(data[:,:,1]; xcenter=xcenter, ycenter=ycenter)
+
+    itrX = xcenter ? (1-nx÷2:nx÷2) : (0:nx)
+    itrY = ycenter ? (1-ny÷2:ny÷2) : (0:ny)
+
+    itrZ = img == (0,0) ? (1:nz) : img
+    nz = length(itrZ)
+
+    ncols = min(res[1]÷266,nz)
+    nrows = inline ? cld(nz,ncols) : min(cld(nz,ncols),res[2]÷300)
+
+    res = inline ? (res[1],max(res[2],nrows*300)) : res
+
+    (scene, layout) = layoutscene(resolution = res)
+
+    for i=1:nrows
+        for j=1:ncols
+            iz = (i-1)*ncols+j
+            if iz <= nz
+                ax = layout[i,j] = Axis(scene, aspect = 1)
+                ax.titlesize = textsize * 7/5
+                ax.title = "img $(itrZ[iz])"
+                ax.xlabelsize = textsize * 6/5
+                ax.ylabelsize = textsize * 6/5
+                ax.xlabel = "x (pixel)"
+                ax.ylabel = "y (pixel)"
+                ax.xticklabelsize = textsize
+                ax.yticklabelsize = textsize
+                ax.xticks = xticks
+                ax.yticks = yticks
+                hm = heatmap!(ax, itrX, itrY, data[:,:,itrZ[iz]])
+                hm.colormap = colormap
+                hm.colorrange = (valmin,valmax)
+            end
+        end
+        cb = layout[i,ncols+1] = Colorbar(scene, width = 10, limits=(valmin,valmax), colormap = colormap, flipaxis = false)
+        cb.ticklabelsize = textsize
+        cb.labelsize = textsize * 6/5
+        cb.label = "intensity"
+        cb.flipaxis = true
+    end
+
+    strNote = " Note: output truncated at image $(ncols*nrows) (limited by specified screen resolution)"
+    footnote = nz > ncols*nrows ? footnote * strNote  : footnote
+    lblFoot = layout[nrows+1,:] = Label(scene, footnote, textsize = textsize * 6/5)
+    supertitle = layout[0,:] = Label(scene, filnam, textsize = textsize * 2 , color = (:black, 0.25))
+
+    return scene
+
+end
+
 function _test_saturation(data; bitchip::Int=14, bitpix::Int=32, bzero::Int=0, numkin::Int=1)
 
     bitchip_sat::Int = (2^bitchip - 1) * numkin
