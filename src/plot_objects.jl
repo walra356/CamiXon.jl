6692e1178a46b0e878cafb594a41a435bb7f6345
Jook Walraven <69215586+walra356@users.noreply.github.com>
2021-05-02 21:09:28 +0200

    Update plot_objects.jl
    added Plot2Dobject

--- src/plot_objects.jl
+++ src/plot_objects.jl
@@ -1,3 +1,14 @@
+"""
+    Plot2Dset
+
+Plot object to decompose the names of .fits files.
+Fields:
+* `.aspect::Real`: aspect ratio
+* `.center::Tuple{Bool,Bool}`: centering true/false of ticks on x/y axes
+* `.ticks::Tuple{AbstractRange,AbstractRange}`: ticks range on x/y axes
+* `.title::String`:  title of plot
+* `.labels::Tuple{String,String}`: labels on x/y axes
+"""
 struct Plot2Dset
 
     aspect::Real
