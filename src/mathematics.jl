bb442cb75cd4f070254482ccaf657d690a4b3a8a
= <walra356@planet.nl>
2021-07-11 22:57:00 +0200

    latex

--- src/mathematics.jl
+++ src/mathematics.jl
@@ -268,7 +268,7 @@ end
 
 Polynomial of degree `d = length(c)-1` defined by the elements of array `c[1:d+1]`:
 
-    polynom(c,x) = c[1] + c[2] x + ... + c[d+1] xᵈ
+``polynom(c,x) = c[1] + c[2] x + ... + c[d+1] xᵈ``
 
 ### Examples:
 ```
@@ -291,5 +291,5 @@ function polynom(c::Vector{T}, x::T) where T<:Real
     end
 
     return LinearAlgebra.dot(c, X)
-    
+
 end
