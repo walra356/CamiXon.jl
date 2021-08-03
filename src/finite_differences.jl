93e5ce4f3b0b860176e9e29f12e3b8e6e4baf631
= <walra356@planet.nl>
2021-08-04 01:31:42 +0200

    patch 0 by 1

--- src/finite_differences.jl
+++ src/finite_differences.jl
@@ -104,7 +104,7 @@ analytic function at offset position ``0\le x\le -k``,
 ```math
 f[n+x] =\sum_{p=0}^{k}l_p(x)\nabla^pf[n] = \sum_{j=0}^{k}r_j^k(x)f[n-j],
 ```
-where ``l_0\equiv 0`` and
+where ``l_0\equiv 1`` and
 ```math
 l_p(x) = x(x+1)(x+2)\cdots(x+p-1)/p!,
 ```
