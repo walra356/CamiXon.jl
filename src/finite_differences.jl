7a61ae4f297bb57464b878a9ea97786a4fd54ccd
= <walra356@planet.nl>
2021-08-03 21:05:53 +0200

    @doc raw"""

--- src/finite_differences.jl
+++ src/finite_differences.jl
@@ -1,13 +1,13 @@
-"""
+@doc raw""""""
     f_diff_weight(k, j)
 
 Weight coefficient
 
-``c_{j}^{k}=(-1)^{j}\\binom{k}{j},``
+``c_{j}^{k}=(-1)^{j}\binom{k}{j},``
 
-of the ``k^{th}``-order finite difference operator ``\\nabla^k`` and corresponding to the function value ``f[n-j]``.
+of the ``k^{th}``-order finite difference operator ``\nabla^k`` and corresponding to the function value ``f[n-j]``.
 #### Example:
-```@docs
+```
 k = 5; i = 3
 f_diff_weight(k, i)
  -10
@@ -17,13 +17,13 @@ f_diff_weight(k::Int, i::Int) = iseven(i) ? Base.binomial(k,i) : -Base.binomial(
 
 
 
-"""
+@doc raw"""
     f_diff_weights(k)
 
-Weight coefficients ``c_0^k,\\ \\ldots,\\ c_k^k`` corresponding to the function values
-``f[n],\\ \\ldots,\ f[n-k]``, for the ``k^{th}``-order finite difference operator ``\\nabla^k``.
+Weight coefficients ``c_0^k,\ \ldots,\ c_k^k`` corresponding to the function values
+``f[n],\ \ldots,\ f[n-k]``, for the ``k^{th}``-order finite difference operator ``\nabla^k``.
 #### Example:
-```@docs
+```
 k = 3
 f_diff_weights(k)
 4-element Vector{Int64}:
@@ -37,14 +37,14 @@ f_diff_weights(k::Int) = [f_diff_weight(k, i) for i=0:k]
 
 
 
-"""
+@doc raw"""
     f_diff_weights_array(kmax)
 
-Weight coefficients ``c_0^k,\\ \\ldots,\\ c_k^k`` corresponding to the function values
-``f[n],\\ \\ldots,\ f[n-k]``, for the ``k^{th}``-order finite difference operators
-``\\nabla^0,\\ \\ldots,\ \\nabla^k``.
+Weight coefficients ``c_0^k,\ \ldots,\ c_k^k`` corresponding to the function values
+``f[n],\ \ldots,\ f[n-k]``, for the ``k^{th}``-order finite difference operators
+``\nabla^0,\ \ldots,\ \nabla^k``.
 #### Example:
-```@docs
+```
 kmax = 3
 ∇ = f_diff_weights_array(kmax)
 4-element Vector{Vector{Int64}}:
