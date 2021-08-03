87a6f6e9d2ed9562569549272a2f3d519edf135b
= <walra356@planet.nl>
2021-08-03 22:51:15 +0200

    update finite_differences

--- src/finite_differences.jl
+++ src/finite_differences.jl
@@ -22,8 +22,9 @@ f_diff_weight(k::Int, i::Int) = iseven(i) ? Base.binomial(k,i) : -Base.binomial(
 
 Weight coefficients ``[c_0^k,\ \ldots,\ c_k^k]`` defining the ``k^{th}``-order finite difference operator,
 ```math
-\nabla^k f[n] = \sum_{j=0}^{k} c_i^kf[n-j].
+\nabla^k f[n] = \sum_{j=0}^{k} c_i^kf[n-j],
 ```
+where ``f[n], ...,f[n-k]`` are elements of a tabulated anaytic function.
 #### Example:
 ```@docs
 k = 3
@@ -42,7 +43,7 @@ f_diff_weights(k::Int) = [f_diff_weight(k, i) for i=0:k]
 @doc raw"""
     f_diff_weights_array(kmax::Int)
 
-Collection of weight coefficients ``c_0^k,\ \ldots,\ c_k^j`` defining the finite difference operators ``\nabla^j`` with ``j\in0\ \ldots,\ k``.
+Collection of weight coefficients ``[c_0^k,\ \ldots,\ c_j^k]`` defining the finite difference operator ``\nabla^j``  ``(0\le j\le k)``.
 #### Example:
 ```@docs
 kmax = 3
@@ -61,12 +62,10 @@ f_diff_weights_array(kmax::Int) = [f_diff_weights(k)  for k=0:kmax]
 
 Summation weights ``[b_0^k,\ ,\ldots,\ b_k^k]`` corresponding to the expansion coefficients
 ``[a_0^k,\ ,\ldots,\ a_k^k]`` of a ``k^{th}``-order finite-difference expansion,
-
 ```math
 \sum_{p=0}^{k}a_{p}\nabla^{p}f[n]=\sum_{j=0}^{k}b_{j}^{k}f[n-j],
 ```
-
-where ``f[n], ...,f[n-k]`` are ``k+1`` values of a tabulated anaytic function f.
+where ``f[n], ...,f[n-k]`` are elements of a tabulated anaytic function.
 #### Example:
 ```
 k=5
