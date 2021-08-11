4527cf0318034310e05f0a051a52112334785ca5
= <walra356@planet.nl>
2021-08-11 15:50:35 +0200

    patched range error summation_range

--- src/finite_differences.jl
+++ src/finite_differences.jl
@@ -245,7 +245,7 @@ function summation_range(n::Int, i::Int, k::Int, μ::Int)
 # ================================================================================================
 #   summation range for point position i lagrangian interpolation
 # ================================================================================================
-     0 ≤ i ≤ n ? m = μ + 1 : error("Error: position index i outside index range 0 ≤ i ≤ n")
+     0 ≤ i ≤ n*μ ? m = μ + 1 : error("Error: position index i outside index range 0 ≤ i ≤ n")
      return i < (n-1-k)*m  ? UnitRange(i÷m+1,i÷m+k+1) : UnitRange(n-k,n)
 end
 
