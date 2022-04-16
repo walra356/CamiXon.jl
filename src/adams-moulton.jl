2a194aeccb3973ce1df796291b229363b76a4191
= <walra356@planet.nl>
2022-04-17 01:22:12 +0200

    include atom.jl

--- src/adams-moulton.jl
+++ src/adams-moulton.jl
@@ -1,3 +1,4 @@
+include atom.jl
 # ================== Pos(Na, Nlctp, Nmin, Nuctp, Nb, N, nodes) =================
 """
     Pos(Na::Int, Nlctp::Int, Nmin::Int, Nuctp::Int, Nb::Int, N::Int, nodes::Int)
