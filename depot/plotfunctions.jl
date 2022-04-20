568b2c9b9ae4ec1f6cab450b312dd8844f18bc07
= <walra356@planet.nl>
2022-04-20 16:33:59 +0200

    patch plotfunctions

--- depot/plotfunctions.jl
+++ depot/plotfunctions.jl
@@ -2,6 +2,10 @@
 
 function plot_gridfunction(itr::UnitRange{Int}, grid::Grid{T}; title="") where T <: Number
 
+    N = grid.N
+    1 ≤ itr.start ≤ N || error("Error: Nstart outside index range 1:$(N)")
+    1 ≤ itr.stop  ≤ N || error("Error: Nstop outside index range 1:$(N)")
+
     strID = length(title) > 0 ? title : grid.name * " grid"
 
     r = grid.r
@@ -44,32 +48,32 @@ end
 function find_energy_interval(Emin, Emax, def::Def{T}) where T <: Number
 # =============================================================================
 #  find indices of energy interval on potential curve
-# =============================================================================            
+# =============================================================================
     Nb = def.pos.Nb
     v = def.pot
     s = def.scr
-    
+
     pot = v .+ s
-    
+
     Emin = myconvert(T, Emin)
     Emax = myconvert(T, Emax)
     Eref = myconvert(T, -5)       # reference for "clipping" of amplitude
-    
+
     n = Nb
     while Emin < pot[n]
         n > 1 ? n -= 1 : break
     end
-    
+
     Nmax = n
-    
+
     while Eref < pot[n] < Emax
         n > 1 ? n -= 1 : break
     end
-    
-    Nmin = n ≠ Nmax ? n : Nmax÷2 
-    
+
+    Nmin = n ≠ Nmax ? n : Nmax÷2
+
     return Nmin, Nmax
-    
+
 end
 # .............................................................................
 function plot_potentials(E::T, grid::Grid{T}, def::Def{T}) where T <: Number
@@ -134,7 +138,7 @@ function plot_potentials(E::T, grid::Grid{T}, def::Def{T}) where T <: Number
     lines!(ax4, [r[1],r[N]], [0,0], markersize = 1, color=:Grey)
     lines!(ax4, [r[1],r[N]], [E,E], markersize = 1, color=:Green)
     E < pot[end] ? scatter!(ax4, (r[Nuctp], E), markersize = 5, color=:Black) : false
-    
+
     lines!(ax5, itr, pot[itr], markersize = 1)
     scatter!(ax5, itr, pot[itr], markersize = 3)
     lines!(ax5, [1,N], [0,0], markersize = 1, color=:Grey)
@@ -150,7 +154,7 @@ function plot_potentials(E::T, grid::Grid{T}, def::Def{T}) where T <: Number
 
     str = string(r[Nuctp])
     lst = min(13, length(str))
-    
+
     println("Nlctp = $(Nlctp), Nmin = $(Nmin), Nuctp = $(Nuctp) (Ructp = " * str[1:lst] * " a.u.)")
 
     return fig
@@ -163,156 +167,156 @@ function _zone(n::Int, def::Def)
 
       Na = def.pos.Na
     Nuctp = def.pos.Nuctp
-      Nb = def.pos.Nb  
+      Nb = def.pos.Nb
        N = def.pos.N
-    
+
        0 < n ≤ Na     && return 1
-      Na < n ≤ Nuctp   && return 2 
+      Na < n ≤ Nuctp   && return 2
     Nuctp < n ≤ Nb     && return 3
       Nb < n ≤ N      && return 4
-    
+
     error("Error: index outside of range 1:$(N)")
-    
+
 end
 
 function _zone_color(zone::Int)
-    
+
     zone == 1 && return :Black
     zone == 2 && return :Blue
     zone == 3 && return :Red
     zone == 4 && return :Black
-    
+
     error("Error: color zone index outside of range 1:4")
-    
+
 end
 
 function _zone_markersize(zone::Int)
-    
+
     zone == 1 && return 2.5
     zone == 2 && return 1.5
     zone == 3 && return 1.5
     zone == 4 && return 2.5
-    
+
     error("Error: color zone index outside of range 1:4")
-    
+
 end
 
 function _scatterX!(ax, r, X, itr, def)
 
       Na = def.pos.Na
     Nuctp = def.pos.Nuctp
-      Nb = def.pos.Nb    
+      Nb = def.pos.Nb
        N = def.pos.N
-    
+
     N1 = itr[1]
     N2 = itr[end]
-    
+
     A = _zone(N1, def)
     B = _zone(N2, def)
-    
+
     if B == A
         itrA = itr
-        clrA = _zone_color(A)        
-        mkrA = _zone_markersize(A)
-        scatter!(ax, r[itrA], X[itrA], markersize = mkrA, color=clrA) 
-    elseif B == A + 1
-        itrA = A == 1 ? (N1:Na) : A == 2 ? (N1:Nuctp)   : A == 3 ? (N1:Nb)   : 0:0   
-        itrB = B == 2 ? (Na+1:N2) : B == 3 ? (Nuctp+1:N2) : B == 4 ? (Nb+1:N2) : 0:0        
         clrA = _zone_color(A)
-        clrB = _zone_color(B)        
-        mkrA = _zone_markersize(A)        
-        mkrB = _zone_markersize(B)
-        scatter!(ax, r[itrA], X[itrA], markersize = mkrA, color=clrA) 
-        scatter!(ax, r[itrB], X[itrB], markersize = mkrB, color=clrB) 
-    elseif B == A + 2  
-        C = A + 1
-        itrA = A == 1 ? (N1:Na) : A == 2 ? (N1:Nuctp) : 0:0    
-        itrC = C == 2 ? (Na+1:Nuctp) : C == 3 ? (Nuctp+1:Nb) : 0:0 
-        itrB = B == 3 ? (Nuctp+1:N2) : B == 4 ? (Nb+1:N2) : 0:0       
+        mkrA = _zone_markersize(A)
+        scatter!(ax, r[itrA], X[itrA], markersize = mkrA, color=clrA)
+    elseif B == A + 1
+        itrA = A == 1 ? (N1:Na) : A == 2 ? (N1:Nuctp)   : A == 3 ? (N1:Nb)   : 0:0
+        itrB = B == 2 ? (Na+1:N2) : B == 3 ? (Nuctp+1:N2) : B == 4 ? (Nb+1:N2) : 0:0
         clrA = _zone_color(A)
         clrB = _zone_color(B)
-        clrC = _zone_color(C)        
-        mkrA = _zone_markersize(A)        
-        mkrB = _zone_markersize(B)        
-        mkrC = _zone_markersize(C)
+        mkrA = _zone_markersize(A)
+        mkrB = _zone_markersize(B)
         scatter!(ax, r[itrA], X[itrA], markersize = mkrA, color=clrA)
-        scatter!(ax, r[itrB], X[itrB], markersize = mkrB, color=clrB) 
-        scatter!(ax, r[itrC], X[itrC], markersize = mkrC, color=clrC) 
-    else
+        scatter!(ax, r[itrB], X[itrB], markersize = mkrB, color=clrB)
+    elseif B == A + 2
         C = A + 1
-        D = A + 2
-        itrA = A == 1 ? (N1:Na) : 0:0    
-        itrC = C == 2 ? (Na+1:Nuctp) : 0:0     
-        itrD = D == 3 ? (Nuctp+1:Nb) : 0:0 
-        itrB = B == 4 ? (Nb+1:N2) : 0:0       
+        itrA = A == 1 ? (N1:Na) : A == 2 ? (N1:Nuctp) : 0:0
+        itrC = C == 2 ? (Na+1:Nuctp) : C == 3 ? (Nuctp+1:Nb) : 0:0
+        itrB = B == 3 ? (Nuctp+1:N2) : B == 4 ? (Nb+1:N2) : 0:0
         clrA = _zone_color(A)
         clrB = _zone_color(B)
         clrC = _zone_color(C)
-        clrD = _zone_color(D)         
-        mkrA = _zone_markersize(A)        
-        mkrB = _zone_markersize(B)        
-        mkrC = _zone_markersize(C)        
+        mkrA = _zone_markersize(A)
+        mkrB = _zone_markersize(B)
+        mkrC = _zone_markersize(C)
+        scatter!(ax, r[itrA], X[itrA], markersize = mkrA, color=clrA)
+        scatter!(ax, r[itrB], X[itrB], markersize = mkrB, color=clrB)
+        scatter!(ax, r[itrC], X[itrC], markersize = mkrC, color=clrC)
+    else
+        C = A + 1
+        D = A + 2
+        itrA = A == 1 ? (N1:Na) : 0:0
+        itrC = C == 2 ? (Na+1:Nuctp) : 0:0
+        itrD = D == 3 ? (Nuctp+1:Nb) : 0:0
+        itrB = B == 4 ? (Nb+1:N2) : 0:0
+        clrA = _zone_color(A)
+        clrB = _zone_color(B)
+        clrC = _zone_color(C)
+        clrD = _zone_color(D)
+        mkrA = _zone_markersize(A)
+        mkrB = _zone_markersize(B)
+        mkrC = _zone_markersize(C)
         mkrD = _zone_markersize(D)
         scatter!(ax, r[itrA], X[itrA], markersize = mkrA, color=clrA)
-        scatter!(ax, r[itrB], X[itrB], markersize = mkrB, color=clrB) 
-        scatter!(ax, r[itrC], X[itrC], markersize = mkrC, color=clrC) 
-        scatter!(ax, r[itrD], X[itrD], markersize = mkrD, color=clrD)  
+        scatter!(ax, r[itrB], X[itrB], markersize = mkrB, color=clrB)
+        scatter!(ax, r[itrC], X[itrC], markersize = mkrC, color=clrC)
+        scatter!(ax, r[itrD], X[itrD], markersize = mkrD, color=clrD)
     end
-    
+
     N1 ≤ Nuctp ≤ N2 ? scatter!(ax, (r[Nuctp], X[Nuctp]), markersize = 5, color=:Black) : false
-    
+
 end
 
 # =========================================================================================================================
 
 function plot_endpoints(Z::Vector{Complex{T}}, grid::Grid{T}, def::Def{T}) where T<:Real
-    
+
        r = grid.r
        k = def.k
-    
+
       Na = k+1
     Nmin = def.pos.Nmin
     Nuctp = def.pos.Nuctp
-       N = def.pos.N 
+       N = def.pos.N
       Nb = N-k
      pos = [Nmin, Na, Nuctp, Nb, N]
-    
+
     itr1 = 1:Na+k
     itr2 = Nb-k:N
-    
+
     symbol = def.atom.symbol
       name = def.orbit.name
-    
+
     fig = Figure()
-    
-    attr1 = set_attributes(fig; title = "OUTSCH: χ(r) on Grid[$(itr1)]", xlabel = "r (a.u.)", ylabel = "χ(r)") 
-    attr2 = set_attributes(fig; title = "INSCH:  χ(r) on Grid[$(itr2)]", xlabel = "r (a.u.)", ylabel = "χ(r)")  
-    attr3 = set_attributes(fig; title = "OUTSCH: dχ/dr on Grid[$(itr1)]", xlabel = "r (a.u.)", ylabel = "dχ/dr") 
-    attr4 = set_attributes(fig; title = "INSCH:  dχ/dr on Grid[$(itr2)]", xlabel = "r (a.u.)", ylabel = "dχ/dr") 
-    
-    ax0 = Label(fig; text = symbol * ": " * name * " - Endpoints", textsize = 24,  color=:gray)                                              
-    ax1 = Axis(fig; attr1...)                            # create axes, add atrributes                                        
-    ax2 = Axis(fig; attr2...)                            # create axes, add atrributes                                      
-    ax3 = Axis(fig; attr3...)                            # create axes, add atrributes                                        
-    ax4 = Axis(fig; attr4...)                            # create axes, add atrributes  
-    
+
+    attr1 = set_attributes(fig; title = "OUTSCH: χ(r) on Grid[$(itr1)]", xlabel = "r (a.u.)", ylabel = "χ(r)")
+    attr2 = set_attributes(fig; title = "INSCH:  χ(r) on Grid[$(itr2)]", xlabel = "r (a.u.)", ylabel = "χ(r)")
+    attr3 = set_attributes(fig; title = "OUTSCH: dχ/dr on Grid[$(itr1)]", xlabel = "r (a.u.)", ylabel = "dχ/dr")
+    attr4 = set_attributes(fig; title = "INSCH:  dχ/dr on Grid[$(itr2)]", xlabel = "r (a.u.)", ylabel = "dχ/dr")
+
+    ax0 = Label(fig; text = symbol * ": " * name * " - Endpoints", textsize = 24,  color=:gray)
+    ax1 = Axis(fig; attr1...)                            # create axes, add atrributes
+    ax2 = Axis(fig; attr2...)                            # create axes, add atrributes
+    ax3 = Axis(fig; attr3...)                            # create axes, add atrributes
+    ax4 = Axis(fig; attr4...)                            # create axes, add atrributes
+
       lines!(ax1, r[itr1], real(Z[itr1]), markersize = 1, color=:gray90)
     _scatterX!(ax1, r, real(Z), itr1, def)
-      lines!(ax2, r[itr2], real(Z[itr2]), markersize = 1, color=:gray90)  
+      lines!(ax2, r[itr2], real(Z[itr2]), markersize = 1, color=:gray90)
     _scatterX!(ax2, r, real(Z), itr2, def)
-      lines!(ax3, r[itr1], imag(Z[itr1]), markersize = 2, color=:gray90)  
+      lines!(ax3, r[itr1], imag(Z[itr1]), markersize = 2, color=:gray90)
     _scatterX!(ax3, r, imag(Z), itr1, def)
-      lines!(ax4, r[itr2], imag(Z[itr2]), markersize = 2, color=:gray90)  
+      lines!(ax4, r[itr2], imag(Z[itr2]), markersize = 2, color=:gray90)
     _scatterX!(ax4, r, imag(Z), itr2, def)
-    
+
     fig[1,1] = ax1                                      # create layout and show figure
     fig[1,2] = ax2                                      # create layout and show figure
     fig[2,1] = ax3                                      # create layout and show figure
     fig[2,2] = ax4                                      # create layout and show figure
     fig[0,:] = ax0
-    
+
     return fig
-    
+
 end
 
 # ===============================================================================
@@ -322,54 +326,54 @@ function plot_wavefunction(Nstart::Int, Nstop::Int, E::T, grid::Grid{T}, def::De
         k = def.k
        Na = def.pos.Na
     Nuctp = def.pos.Nuctp
-       Nb = def.pos.Nb   
+       Nb = def.pos.Nb
         N = def.pos.N
-    
+
     println()
     println("plot_wavefunction: color coding based on Na = $(def.pos.Na), Nuctp = $(def.pos.Nuctp), Nb = $(def.pos.Nb), N = $(def.pos.N)")
-    
-    1 ≤ Nstart ≤ N || error("Error: Nstart outside index range 1:$(N)")    
+
+    1 ≤ Nstart ≤ N || error("Error: Nstart outside index range 1:$(N)")
     1 ≤ Nstop  ≤ N || error("Error: Nstop outside index range 1:$(N)")
-    
-    ylabel1, ylabel2, Z = reduced ? ("χ(r)", "dχ/dr", Z) :  ("ψ(r)", "dψ/dr", wavefunction(r,Z)) 
+
+    ylabel1, ylabel2, Z = reduced ? ("χ(r)", "dχ/dr", Z) :  ("ψ(r)", "dψ/dr", wavefunction(r,Z))
     ylabelA, ylabelB = reduced ? ("χ(n)", "dχ/dr") :  ("ψ(n)", "dψ/dr")
-    
-    header = reduced ? "reduced wavefunction" :  "full wavefunction"  
-    
+
+    header = reduced ? "reduced wavefunction" :  "full wavefunction"
+
     A = _zone(Nstart, def)
     B = _zone(Nstop, def)
 
     P = convert.(Float64,real(Z))
     Q = convert.(Float64,imag(Z))
-    
+
     P0 = P
     Q0 = Q
-    
+
     r = convert.(Float64,r)
     n = [i for i=1:N]
-    
+
     itr0 = Nstart:Nstop
- 
+
     symbol = def.atom.symbol
       name = def.orbit.name
-    
+
     fig = Figure()
-    
-    attr1 = set_attributes(fig; title = ylabel1 * " on Grid[$(itr0)]", xlabel = "r (a.u.)", ylabel = ylabel1)    
-    attr2 = set_attributes(fig; title = ylabelA * " on Grid[$(itr0)]", xlabel = "n", ylabel = ylabelA)  
+
+    attr1 = set_attributes(fig; title = ylabel1 * " on Grid[$(itr0)]", xlabel = "r (a.u.)", ylabel = ylabel1)
+    attr2 = set_attributes(fig; title = ylabelA * " on Grid[$(itr0)]", xlabel = "n", ylabel = ylabelA)
     attr3 = set_attributes(fig; title = ylabel2 * " on Grid[$(itr0)]", xlabel = "r (a.u.)", ylabel = ylabel2)
     attr4 = set_attributes(fig; title = ylabelB * " on Grid[$(itr0)]", xlabel = "n", ylabel = ylabelB)
-    
+
     ax0 = Label(fig; text = symbol * ": " * name, textsize = 24,  color=:gray)
-    ax1 = Axis(fig; attr1...)                                                      
-    ax2 = Axis(fig; attr2...)   
-    ax3 = Axis(fig; attr3...)                                                      
-    ax4 = Axis(fig; attr4...) 
- 
+    ax1 = Axis(fig; attr1...)
+    ax2 = Axis(fig; attr2...)
+    ax3 = Axis(fig; attr3...)
+    ax4 = Axis(fig; attr4...)
+
     lines!(ax1, r[itr0], P[itr0] , markersize = 1, color=:gray90)
     lines!(ax2, n[itr0], P0[itr0], markersize = 1, color=:gray90)
     lines!(ax3, r[itr0], Q[itr0] , markersize = 1, color=:gray90)
-    lines!(ax4, n[itr0], Q0[itr0], markersize = 1, color=:gray90)  
+    lines!(ax4, n[itr0], Q0[itr0], markersize = 1, color=:gray90)
 
     lines!(ax1, r[itr0], P[itr0], markersize = 1, color=:gray90)
     _scatterX!(ax1, r, P, itr0, def)
@@ -383,22 +387,22 @@ function plot_wavefunction(Nstart::Int, Nstop::Int, E::T, grid::Grid{T}, def::De
     lines!(ax4, n[itr0], Q0[itr0], markersize = 1, color=:gray90)
     _scatterX!(ax4, n, Q0, itr0, def)
 
-    fig[1,1] = ax1           
-    fig[1,2] = ax2    
-    fig[2,1] = ax3           
-    fig[2,2] = ax4  
-    fig[0,:] = ax0 
-    
+    fig[1,1] = ax1
+    fig[1,2] = ax2
+    fig[2,1] = ax3
+    fig[2,2] = ax4
+    fig[0,:] = ax0
+
     return fig
-    
+
 end
 function plot_wavefunction(itr::UnitRange{Int}, E::T, grid::Grid{T}, def::Def{T}, Z::Vector{Complex{T}}; reduced=true) where T <: Real
 
     Nstart = itr.start
     Nstop  = itr.stop
-    
+
     return plot_wavefunction(Nstart, Nstop, E, grid, def, Z; reduced)
-    
+
 end
 
 # =========================================================================================================================
@@ -409,15 +413,15 @@ function analyze_wavefunction(Z::Vector{Complex{T}}, grid::Grid{T}, def::Def{T};
       pos = def.pos
        Na = def.pos.Na
     Nuctp = def.pos.Nuctp
-       Nb = def.pos.Nb   
+       Nb = def.pos.Nb
         N = def.pos.N
-        
-    ylabel1, ylabel2, Z = reduced ? ("χ(r)", "dχ/dr", Z) :  ("ψ(r)", "dψ/dr", wavefunction(r,Z)) 
-    
-    header = reduced ? "reduced wavefunction" :  "full wavefunction"    
+
+    ylabel1, ylabel2, Z = reduced ? ("χ(r)", "dχ/dr", Z) :  ("ψ(r)", "dψ/dr", wavefunction(r,Z))
+
+    header = reduced ? "reduced wavefunction" :  "full wavefunction"
     P = real(Z)
     Q = imag(Z)
-    
+
     itr0 = 1:N
     itr1 = 1:Nuctp
     itr2 = Nuctp-9:Nuctp+9
@@ -425,55 +429,55 @@ function analyze_wavefunction(Z::Vector{Complex{T}}, grid::Grid{T}, def::Def{T};
     itr4 = 1:Na+10
     itr5 = itr2
     itr6 = Nb-9:N
-    
+
     symbol = def.atom.symbol
       name = def.orbit.name
-    
+
     fig = Figure()
-    
-    attr1 = set_attributes(fig; title = "Grid[$(itr0)]", xlabel = "r (a.u.)", ylabel = ylabel1) 
+
+    attr1 = set_attributes(fig; title = "Grid[$(itr0)]", xlabel = "r (a.u.)", ylabel = ylabel1)
     attr2 = set_attributes(fig; title = "Grid[$(itr2)]", xlabel = "r (a.u.)", ylabel = ylabel1)
     attr3 = set_attributes(fig; title = "Grid[$(itr0)]", xlabel = "r (a.u.)", ylabel = ylabel2)
-    attr4 = set_attributes(fig; title = "Grid[$(itr4)]", xlabel = "r (a.u.)", ylabel = ylabel1) 
+    attr4 = set_attributes(fig; title = "Grid[$(itr4)]", xlabel = "r (a.u.)", ylabel = ylabel1)
     attr5 = set_attributes(fig; title = "Grid[$(itr5)]", xlabel = "r (a.u.)", ylabel = ylabel2)
     attr6 = set_attributes(fig; title = "Grid[$(itr6)]", xlabel = "r (a.u.)", ylabel = ylabel1)
-    
+
     ax0 = Label(fig; text = symbol * ": " * name * " - " * header, textsize = 24,  color=:gray)
     ax1 = Axis(fig; attr1...)
-    ax2 = Axis(fig; attr2...)                                             
+    ax2 = Axis(fig; attr2...)
     ax3 = Axis(fig; attr3...)
-    ax4 = Axis(fig; attr4...)                                       
-    ax5 = Axis(fig; attr5...)                                      
+    ax4 = Axis(fig; attr4...)
+    ax5 = Axis(fig; attr5...)
     ax6 = Axis(fig; attr6...)
-    
+
     lines!(ax1, r, P, markersize = 1, color=:gray90)
     _scatterX!(ax1, r, P, itr0, def)
-    
+
     lines!(ax2, r[itr2], P[itr2], markersize = 1, color=:gray90)
     _scatterX!(ax2, r, P, itr2, def)
-    
+
     lines!(ax3, r, Q, markersize = 1, color=:gray90)
     _scatterX!(ax3, r, Q, itr0, def)
-       
+
     lines!(ax4, r[itr4], P[itr4]/P[Na], markersize = 1, color=:gray90)
     _scatterX!(ax4, r, P/P[Na], itr4, def)
-    
+
     lines!(ax5, r[itr5], Q[itr5], markersize = 1, color=:gray90)
     _scatterX!(ax5, r, Q, itr5, def)
-    
+
     lines!(ax6, r[itr6], P[itr6], markersize = 1, color=:gray90)
     _scatterX!(ax6, r, P, itr6, def)
-    
-    fig[1,1] = ax1    
-    fig[1,2] = ax2                
-    fig[1,3] = ax3     
-    fig[2,1] = ax4           
-    fig[2,2] = ax5           
-    fig[2,3] = ax6  
-    fig[0,:] = ax0 
-    
+
+    fig[1,1] = ax1
+    fig[1,2] = ax2
+    fig[1,3] = ax3
+    fig[2,1] = ax4
+    fig[2,2] = ax5
+    fig[2,3] = ax6
+    fig[0,:] = ax0
+
     return fig
-    
+
 end
 
 println("Included:
@@ -482,6 +486,3 @@ plot_potentials(E, grid, def)
 plot_wavefunction(Nstart, Nstop, E, grid, def, Z; reduced=true)
 plot_endpoints(Z, grid, def)
 analyze_wavefunction(Z, grid, def; reduced=true)")
-
-
-
