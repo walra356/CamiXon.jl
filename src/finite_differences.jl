fbfa7d32bc70912eadf85a3525f9c5b26bc8d996
= <walra356@planet.nl>
2022-05-05 20:12:20 +0200

    work on docs

--- src/finite_differences.jl
+++ src/finite_differences.jl
@@ -15,7 +15,8 @@ c(5,3)
  -10
 ```
 """
-f_diff_weight(k::Int, j::Int) = Base.iseven(j) ? Base.binomial(k,j) : -Base.binomial(k,j)
+f_diff_weight(k::Int, j::Int) = Base.iseven(j) ? Base.binomial(k,j) :
+                                                -Base.binomial(k,j)
 
 # ==============================================================================
 
@@ -31,11 +32,13 @@ Applications:
 
 The *forward difference* summation is
 ```math
-\Delta^k f[n]=[c_{k}^{k},\thinspace c_{k-1}^{k},\thinspace\ldots,c_{0}^{k}]\left[\begin{array}{c}
+\Delta^k f[n]=[c_{k}^{k},\, c_{k-1}^{k},\,\ldots,c_{0}^{k}]
+\left[\begin{array}{c}
 f[n]\\
 \vdots\\
 f[n+k]
-\end{array}\right]=\sum_{j=0}^{k} c_{k-j}^kf[n+j].
+\end{array}\right]
+=\sum_{j=0}^{k} c_{k-j}^kf[n+j].
 ```
 
 This convention applies to *analytical* functions, ``f``, tabulated
@@ -45,11 +48,13 @@ in *forward* order as ``f[n], ...,f[n+k]``.
 
 The *backward difference* summation is
 ```math
-\nabla^{k}f[n]=[c_{k}^{k},\thinspace c_{k-1}^{k},\thinspace\ldots,c_{0}^{k}]\left[\begin{array}{c}
+\nabla^{k}f[n]=[c_{k}^{k},\,c_{k-1}^{k},\,\ldots,c_{0}^{k}]
+\left[\begin{array}{c}
 f[n-k]\\
 \vdots\\
 f[n]
-\end{array}\right]=\sum_{j=0}^{k}c_{k-j}^kf[n-k+j].
+\end{array}\right]
+=\sum_{j=0}^{k}c_{k-j}^kf[n-k+j].
 ```
 
 This convention applies to *analytical* functions, ``f``, tabulated
@@ -83,9 +88,11 @@ Collection of finite difference weight vectors, ``c^0,\ \ldots,\ c^k``, where
 
 Application in [`Finite-difference-expansions`](@ref)
 
-`f_diff_weights_array(kmax)` ``\rightarrow\ [\ c^0,\ c^1,\ \ldots,\ c^{kmax} ]``,
+`f_diff_weights_array(kmax)`
+``\rightarrow\ [\ c^0,\ c^1,\ \ldots,\ c^{kmax} ]``,
 
-where [`f_diff_weights(k)`](@ref)``\rightarrow\ c^k ≡ [c_k^k,\ c_1^k,\ldots,\ c_0^k]``.
+where [`f_diff_weights(k)`](@ref)
+``\rightarrow\ c^k ≡ [c_k^k,\ c_1^k,\ldots,\ c_0^k]``.
 #### Example:
 ```
 kmax = 3
@@ -102,17 +109,64 @@ f_diff_weights_array(kmax::Int) = [CamiXon.f_diff_weights(k)  for k=0:kmax]
 # ==============================================================================
 
 @doc raw"""
-    backward_diff_expansion_weights(β, ∇)
+    forward_diff_expansion_weights(α, Δ)
 
-Weight vector ``b^k ≡ [B_k^k,\ ,\ldots,\ B_0^k]`` corresponding to the
-expansion coefficients ``[β_0^k,\ ,\ldots,\ β_k^k]`` of the ``k^{th}``-order
-finite-difference expansion,
+Weight vector ``F^k ≡ [F_k^k,\ ,\ldots,\ F_0^k]`` corresponding to the
+expansion coefficients ``[α_0^k,\ ,\ldots,\ α_k^k]`` of the ``k^{th}``-order
+backwards difference expansion,
 
 ```math
-\sum_{p=0}^{k}α_{p}\nabla^{p}f[n]=\sum_{j=0}^{k}b^k[j]f[n-k+j],
+\sum_{p=0}^{k}α_{p}Δ^{p}f[n]
+=\sum_{j=0}^{k}F^k_j f[n-k+j],
 ```
 
-where ``b^k[j] \equiv B_{k-j}^k`` and ``f[n-k], ...,f[n]`` are elements of the
+where ``f[n-k], ...,f[n]`` are elements of the
+analytic function ``f`` tabulated in *forward* order. Note the difference in
+ordering between the finite-difference expansion *coefficients*,
+``α_{0},\ \ldots,\ α_{k}``, and the finite-difference expansion *weights*,
+``F_k^{k},\ \ldots,\ F_0^{k}``. Note further the difference in ``k`` dependence:
+the *weights*, ``F_j^k``, are ``k``*-dependent*, whereas the *coefficients*,
+``α_j``, are not.
+#### Example:
+```
+k=5
+Δ = f_diff_weights_array(k)
+α = UnitRange(0,k)
+F = backward_diff_expansion_weights(α, Δ)
+6-element Vector{Int64}:
+  15
+ -55
+  85
+ -69
+  29
+  -5
+```
+"""
+function forward_diff_expansion_weights(α, Δ)
+# ==============================================================================
+#   function weights of finite-difference summation
+# ==============================================================================
+    k = Base.length(α)-1
+
+    return [sum([coeffs[1+p] * ∇[1+p][1+p-j] for p=j:k]) for j=0:k]
+
+end
+
+# ================ backward_diff_expansion_weights(β, ∇) =======================
+
+@doc raw"""
+    backward_diff_expansion_weights(β, ∇)
+
+Weight vector ``B^k ≡ [B_k^k,\ ,\ldots,\ B_0^k]`` corresponding to the
+expansion coefficients ``[β_0^k,\ ,\ldots,\ β_k^k]`` of the ``k^{th}``-order
+backwards difference expansion,
+
+```math
+\sum_{p=0}^{k}β_{p}\nabla^{p}f[n]
+=\sum_{j=0}^{k}B^k_j f[n-k+j],
+```
+
+where ``f[n-k], ...,f[n]`` are elements of the
 analytic function ``f`` tabulated in *forward* order. Note the difference in
 ordering between the finite-difference expansion *coefficients*,
 ``β_{0},\ \ldots,\ β_{k}``, and the finite-difference expansion *weights*,
@@ -135,11 +189,11 @@ B = backward_diff_expansion_weights(β, ∇)
 ```
 """
 function backward_diff_expansion_weights(β, ∇)
-# ======================================================================================
+# ==============================================================================
 #   function weights of finite-difference summation
-# ======================================================================================
+# ==============================================================================
     k = Base.length(β)-1
-    #return [sum([coeffs[1+p] * ∇[1+p][1+p-j] for p=j:k]) for j=0:k]
+
     return [Base.sum([β[1+p] * ∇[1+p][1+p-j] for p=j:k]) for j=k:-1:0]
 end
 
@@ -148,14 +202,16 @@ end
 @doc raw"""
     f_diff_expansion_coeffs_lagrange(k::Int, x::T) where T<:Real
 
-Finite-difference expansion coefficient vector ``[l_0(x),\ \ldots,\ l_p(x)]`` defining
-``k^{th}``-order lagrangian interpolation of the tabulated analytic function ``f(n+x)``
-at offset position ``x`` with respect to position ``n``,
+Finite-difference expansion coefficient vector ``[l_0(x),\ \ldots,\ l_p(x)]``
+defining the ``k^{th}``-order lagrangian interpolation of the tabulated
+analytic function ``f(n+x)`` at offset position ``x`` with respect to
+position ``n``,
 ```math
 f[n+x] = (1 - \nabla)^{-x} f[n] \equiv \sum_{p=0}^{\infty}l_p(x)∇^p f[n],
 ```
-where ``l_0\equiv 1`` and ``l_p(x) = x(x+1)(x+2)\cdots(x+p-1)/p!``. Interpolation corresponds to the interval
-``-k\le\ x\le 0``; extrapolation to ``x\ge 0``.
+where ``l_0\equiv 1`` and ``l_p(x) = x(x+1)(x+2)\cdots(x+p-1)/p!``.
+Interpolation corresponds to the interval ``-k\le\ x\le 0``;
+extrapolation to ``x\ge 0``.
 #### Examples:
 ```
 k = 5; x = 1
@@ -164,11 +220,10 @@ l = f_diff_expansion_coeffs_lagrange(k,x); println(l)
 ```
 """
 function f_diff_expansion_coeffs_lagrange(k::Int, x::T) where T<:Real
-# ======================================================================================
-#   f_difference expansion coefficients for lagrange interpolation for position n+x
-# ======================================================================================
+
     l = Base.ones(T,k+1)
-    x == 1 ? l : x ==0 ? (for i=2:k+1; l[i] = 0 end) : (for i=1:k; l[i+1] = l[i]*(x+i-1)/i end)
+    x == 1 ? l : x ==0 ? (for i=2:k+1; l[i] = 0 end) :
+                         (for i=1:k; l[i+1] = l[i]*(x+i-1)/i end)
     return l
 end
 
@@ -177,11 +232,11 @@ end
 @doc raw"""
     summation_range(n, i, k, m)
 
-Summation range for interpolation position ``0\le i/m \le 1`` used in ``k^{th}``-order
-lagrangian interpolation of the anaytic function ``f`` tabulated in forward
-order on a uniform grid of ``n`` points, ``f[1],\ \ldots,\ f[n]``;
-``m`` is the multiplier defining the interpolation grid size.
-
+Summation range for interpolation position ``0\le i/m \le 1`` used
+in ``k^{th}``-order lagrangian interpolation of the anaytic function
+``f`` tabulated in forward order on a uniform grid of ``n`` points,
+``f[1],\ \ldots,\ f[n]``; ``m`` is the multiplier defining the interpolation
+grid size.
 #### Examples:
 ```
 n = 7; k = 2; m = 1
@@ -190,11 +245,14 @@ o = [summation_range(n,i,k,m) for i=0:(n-1)*m]; println(o)
 ```
 """
 function summation_range(n::Int, i::Int, k::Int, m::Int)
-# ================================================================================================
+# ==============================================================================
 #   summation range for point position i lagrangian interpolation
-# ================================================================================================
-      0 ≤ i ≤ n*m || error("Error: position index i outside index range 0 ≤ i ≤ n⋅m")
+# ==============================================================================
+      strErr = "Error: position index i outside index range 0 ≤ i ≤ n⋅m"
+      0 ≤ i ≤ n*m || error(strErr)
+
      return i < (n-1-k)*m  ? UnitRange(i÷m+1,i÷m+k+1) : UnitRange(n-k,n)
+
 end
 
 # ==============================================================================
@@ -204,8 +262,9 @@ end
 
 Finite-difference summation sequences of function values given in forward order
 for use in ``k^{th}``-order lagrangian interpolation of the anaytic function
-``f`` tabulated in forward order on a uniform grid of ``n`` points, ``f[1], ...,f[n]``;
-``m`` is the multiplier defining the interpolation grid size. Each sequence consists of ``k⋅m+1`` function values.
+``f`` tabulated in forward order on a uniform grid of ``n`` points,
+``f[1], ...,f[n]``; ``m`` is the multiplier defining the interpolation grid
+size. Each sequence consists of ``k⋅m+1`` function values.
 #### Example:
 ```
 f = [0,1,2,3,4,5,6]
@@ -215,11 +274,14 @@ o = f_diff_function_sequences(f, k); println(o)
 ```
 """
 function f_diff_function_sequences(f, k::Int, m=1)
-# ================================================================================================
-#   finite-difference function values for interpolation range of lagrangian interpolation
-# ================================================================================================
+# ==============================================================================
+#   finite-difference function values for interpolation range
+#   of lagrangian interpolation
+# ==============================================================================
     n = Base.length(f)
+
     return [f[CamiXon.summation_range(n,i,k,m)] for i=0:(n-1)*m]
+
 end
 
 # ==============================================================================
@@ -239,9 +301,9 @@ domain = 0.0..1.0
 ```
 """
 function lagrange_interpolation(f::Vector{Float64}, domain::ClosedInterval{Float64}; k=3, m=1)
-# ======================================================================================
+# ==============================================================================
 #   lagrangian (k+1)-point interpolation at i interpolation points
-# ======================================================================================
+# ==============================================================================
     n = length(f)
 
     ∇ = CamiXon.f_diff_weights_array(k)
@@ -260,9 +322,10 @@ end
 @doc raw"""
     lagrangian_extrapolation(f::Vector{Float64}, domain::ClosedInterval{Float64}; k=1, e=1, m=1)
 
-``k^{th}``-order lagrangian *extrapolation* up to position ``n+e`` of the analytic function
-``f`` tabulated in forward order at ``n`` points, ``f[1],\ \ldots,\ f[n]``;
-``m`` is the multiplier defining the interpolation grid size.
+``k^{th}``-order lagrangian *extrapolation* up to position ``n+e`` of the
+analytic function ``f`` tabulated in forward order at ``n`` points,
+``f[1],\ \ldots,\ f[n]``; ``m`` is the multiplier defining the interpolation
+grid size.
 #### Example:
 ```
 f = [0.0,1,2,3,4,5,6,7]
@@ -272,15 +335,15 @@ domain = 0.0..1.0
 ```
 """
 function lagrange_extrapolation(f::Vector{Float64}, domain::ClosedInterval{Float64}; k=1, e=1, m=1)
-# ======================================================================================
+# ==============================================================================
 #   lagrangian (k+1)-point interpolation at μ interpolation points
-# ======================================================================================
+# ==============================================================================
     n = Base.length(f)
 
-    ∇ = CamiXon.f_diff_weights_array(k)
-    l = [CamiXon.f_diff_expansion_coeffs_lagrange(k, x) for x=0:1/m:e]
-    w1 = [CamiXon.backward_diff_expansion_weights(l[i], ∇) for i ∈ Base.eachindex(l)]
-    w2 = CamiXon.f_diff_function_sequences(f, k, m)[end]
+    ∇ = f_diff_weights_array(k)
+    l = [f_diff_expansion_coeffs_lagrange(k, x) for x=0:1/m:e]
+    w1 = [backward_diff_expansion_weights(l[i], ∇) for i ∈ Base.eachindex(l)]
+    w2 = f_diff_function_sequences(f, k, m)[end]
 
     ΔX = (domain.right - domain.left)/((n-1)*m)
     X = Base.range(domain.right, domain.right + ΔX * m*e, length=m*e+1)
@@ -290,7 +353,7 @@ function lagrange_extrapolation(f::Vector{Float64}, domain::ClosedInterval{Float
 
 end
 
-# ===================================== f_diff_expansion_coeffs_differentiation(k, x) ====
+# =========================== f_diff_expansion_coeffs_differentiation(k, x) ====
 
 @doc raw"""
     f_diff_expansion_coeffs_differentiation(k::Int, x::T) where T<:Real
@@ -309,9 +372,10 @@ o = f_diff_expansion_coeffs_differentiation(k,x); println(o)
 ```
 """
 function f_diff_expansion_coeffs_differentiation(k::Int, x::T) where T<:Real
-# ======================================================================================
-#   finite difference expansion coeffs for differentiation in interval -k ≤ x ≤ 0
-# ======================================================================================
+# ==============================================================================
+#   finite difference expansion coeffs for differentiation
+#   in interval -k ≤ x ≤ 0
+# ==============================================================================
     a = Base.prepend!([1//i for i=1:k],[0//1])
     b = CamiXon.f_diff_expansion_coeffs_lagrange(k, x)
 
@@ -393,7 +457,8 @@ end
 # ================ f_diff_expansion_coeffs_array_differentiation(k, m) =========
 
 @doc raw"""
-    lagrange_differentiation(f::Vector{Float64}, domain::ClosedInterval{Float64}; k=1, m=1)
+    lagrange_differentiation(f::Vector{Float64},
+                                    domain::ClosedInterval{Float64}; k=1, m=1)
 
 ``k^{th}``-order lagrangian *differentiation* of the analytic function ``f``,
 tabulated in forward order on a uniform grid of ``n`` points, ``f[1],\ \ldots,
@@ -407,17 +472,18 @@ X,Y = lagrange_differentiation(f, domain; k=2, m = 1)
   (0.0:1.0:5.0, [0.0, 2.0, 4.0, 6.0, 8.0, 10.0])
 ```
 """
-function lagrange_differentiation(f::Vector{Float64}, domain::ClosedInterval{Float64}; k=1, m=1)
+function lagrange_differentiation(f::Vector{Float64},
+                             domain::ClosedInterval{Float64}; k=1, m=1)
 # ==============================================================================
 #   lagrangian (k+1)-point differentiation at m interpolation points
 # ==============================================================================
     n = Base.length(f)
 
-    ∇ = CamiXon.f_diff_weights_array(k)
-    l = [CamiXon.f_diff_expansion_coeffs_differentiation(k, x) for x=-k:1/m:0]
-    w = [CamiXon.backward_diff_expansion_weights(l[i], ∇) for i ∈ Base.eachindex(l)]
+    ∇ = f_diff_weights_array(k)
+    l = [f_diff_expansion_coeffs_differentiation(k, x) for x=-k:1/m:0]
+    w = [backward_diff_expansion_weights(l[i], ∇) for i ∈ Base.eachindex(l)]
     w1 = Base.append!(repeat(w[1:m],n-k-1),w)
-    w2 = CamiXon.f_diff_function_sequences(f, k, m)
+    w2 = f_diff_function_sequences(f, k, m)
 
     X = Base.range(domain.left, domain.right, length=(n-1)*m+1)
     Y = (n-1)/(domain.right-domain.left) .*  [w1[i] ⋅ w2[i] for i ∈ Base.eachindex(w1)]
@@ -434,7 +500,8 @@ end
 Weight coefficient vector ``a=[a_1,\cdots,\ a_k]`` of trapeziodal rule
 optimized for functions of polynomial form,
 ```math
-    ∫_0^n f(x) dx = a_1 (f_0+f_n)+\cdots+a_k (f_{k-1}+f_{n-k+1}) + (f_k+\cdots+f_{n-k}),
+    ∫_0^n f(x) dx = a_1 (f_0+f_n) + \cdots + a_k (f_{k-1}+f_{n-k+1})
+                                                         + (f_k+\cdots+f_{n-k}),
 ```
 where ``k`` is *odd*. The rule is exact for polynonials of degree ``d=0,\ 1,
 \cdots,\ k-1``. For ``k=1`` the rule reduces to the ordinary trapezoidal rule.
@@ -448,21 +515,23 @@ without specification of the gcd devisor.
   (3, 24, [9, 28, 23])
   (5, 1440, [475, 1902, 1104, 1586, 1413])
   (7, 120960, [36799, 176648, 54851, 177984, 89437, 130936, 119585])
-  (9, 7257600, [2082753, 11532470, 261166, 16263486, -1020160, 12489922, 5095890, 7783754, 7200319])
+  (9, 7257600, [2082753, 11532470, 261166, 16263486, -1020160, 12489922,
+                                                     5095890, 7783754, 7200319])
 ```
 """
 function trapezoidal_weights(k::Int; rationalize=false, devisor=false)
 # ==============================================================================
 # trapezoidal_weights(k; rationalize=false, devisor=false)
 # ==============================================================================
-    Base.isodd(k) ? true : (k=k+1; println("Warning: k = $(k-1) → $(k) (trapezoidal rule requires odd k)"))
+    Base.isodd(k) ? true :
+    (k=k+1; println("Warning: k = $(k-1) → $(k) (trapezoidal rule requires odd k)"))
 
     l = k - 1
     σ = Base.Matrix{Int}(undef,k,k)
 
     for i=0:k-1
-        σ[i+1,:] = CamiXon.polynom_power([i,-1],l)      # corresponds to coeffs a0,...,ak
-        σ[i+1,1] = σ[i+1,1] + i^l                       # correction of coeff a0
+        σ[i+1,:] = CamiXon.polynom_power([i,-1],l) # corresponds to a0,...ak
+        σ[i+1,1] = σ[i+1,1] + i^l                  # correction of coeff a0
     end
 
     F = CamiXon.faulhaber_polynom(k)
@@ -473,12 +542,13 @@ function trapezoidal_weights(k::Int; rationalize=false, devisor=false)
 
     σ = Base.inv(Base.transpose(σ))
 
-    o = -σ * c  # solving matrix equation results in trapezoidal_weights as real numbers
+    o = -σ * c  # solving matrix equation results in trapezoidal_weights
 
     if rationalize
         a = CamiXon.f_diff_expansion_coeffs_adams_moulton(k)
-        D = Base.denominator(Base.gcd(a))       # trapezoidal_weights divisor == Adams-Moulton devisor
-        o = devisor ? (k, D, Base.round.(Int, o* D)) : Base.round.(Int, o* D) // D              # convert to Rational{Int}
+        D = Base.denominator(Base.gcd(a))       # == Adams-Moulton devisor
+        o = devisor ? (k, D, Base.round.(Int, o* D)) :
+                      Base.round.(Int, o* D) // D     # convert to Rational{Int}
     end
 
     return o
@@ -494,7 +564,8 @@ Integral of the tabulated function ``f=[f_0,\cdots,\ f_n]`` over the `domain`
 ``a..b`` using the optimized trapezoidal rule with endpoint correction by the
 weightsvector `weights`,
 ```math
-    ∫_0^n f(x) dx = a_1 (f_0+f_n)+\cdots+a_k (f_{k-1}+f_{n-k+1}) + (f_k+\cdots+f_{n-k}).
+    ∫_0^n f(x) dx = a_1 (f_0+f_n) + \cdots + a_k (f_{k-1}+f_{n-k+1})
+                                                         + (f_k+\cdots+f_{n-k}).
 ```
 The rule is exact for polynonials of degree ``d=0,\ 1,\cdots,\ k-1``.
 For ``k=1`` the rule reduces to the ordinary trapezoidal rule (weights = [1/2]).
