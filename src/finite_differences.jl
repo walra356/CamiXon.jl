c8fa14187606c9bee77107095c075463c4321f85
= <walra356@planet.nl>
2022-05-30 13:13:56 +0200

    work on docs

--- src/finite_differences.jl
+++ src/finite_differences.jl
@@ -107,7 +107,8 @@ Application:
 [`fdiff_weights_array(k,fwd)`](@ref) →
 `fwd_diffs ≡ ` ``[\bar{c}^0,\ \bar{c}^1,⋯\ \bar{c}^k ]``
 
-[`bwd_diff_weights_array(k,bwd)`](@ref) → `fdiffs ≡ ` ``[c^0,\ c^1,⋯\ c^k ]``
+[`fdiff_weights_array(k,bwd)`](@ref) →
+`bwd_diffs ≡ ` ``[c^0,\ c^1,⋯\ c^k ]``
 
 where ``c^k ← `` [`fdiff_weights(k,bwd)`](@ref) and
 ``\bar{c}^k ← `` [`fdiff_weights(k,fwd)`](@ref)
