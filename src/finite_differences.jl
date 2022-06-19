1be0dbed9ec98b4e6781bc98a7374018aedd7f5c
= <walra356@planet.nl>
2022-06-19 14:31:43 +0200

    work on docs

--- src/finite_differences.jl
+++ src/finite_differences.jl
@@ -316,11 +316,12 @@ end
 @doc raw"""
     fdiff_expansion_coeffs_differentiation(k::Int, x::T) where T<:Real
 
-Finite-difference expansion coefficient vector ``[l_0^{\prime}(x),\ ⋯,
-\ l_p^{\prime}(x)]`` defining ``k^{th}``-order lagrangian *differentiation*
+Finite-difference expansion coefficient vector ``[β_0(x),\ ⋯,\ β_p(x)]``
+defining ``k^{th}``-order lagrangian *differentiation*
 of the tabulated analytic function ``f(n+x)`` at position ``x``,
+    
 ```math
-\frac{df}{dx}[n+x]=\sum_{p=0}^{k}l_{p}^{\prime}(x)∇^{p}f[n]
+\frac{df}{dx}[n+x]=\sum_{p=0}^kβ_p(x)∇^{p}f[n]
 ```
 #### Example:
 ```
