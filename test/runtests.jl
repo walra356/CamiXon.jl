66b9ddc4f99c52e88e7c937173049385ab232429
= <walra356@planet.nl>
2022-10-04 01:04:38 +0200

    work on docs

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,9 +20,7 @@ using Test
     @test get_Na(Z, def) == 8
     @test get_Nb(Z, def) == 92
     @test get_Nuctp(E, def) == 66
-    #@test Z[50] ≈ 0.9161633762690832 + 1.4868742430063544im
-    norm = grid_trapezoidal_integral(real(Z) .^2, 1, grid.N, grid)
-    println("norm = $(norm)")
+    @test grid_trapezoidal_integral(real(Z) .^2, 1, grid.N, grid) == 1.0
     @test sup(-5//2) == "⁻⁵ᐟ²"
     @test sub(-5//2) == "₋₅⸝₂"
     @test frac(-5//2) == "-⁵/₂"
