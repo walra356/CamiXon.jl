94e8c4db7852b6a09fd99c0ab2ea478e79467536
= <walra356@planet.nl>
2022-10-24 12:40:06 +0200

    patch UF

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,8 +14,8 @@ using Test
     adams = castAdams(E, grid, def);
     E, def, adams, Z = adams_moulton_master(E, grid, def, adams; Δν=Value(1,"kHz"), imax=25, msg=false);
     Z1 = hydrogenic_reduced_wavefunction(atom, orbit, grid, def);
-    val = UF(0,Z,grid)[1];
-    @test round(val) == 1
+    #val = UF(0,real(Z),grid)[1];
+    @test round(UF(0,real(Z),grid)[1]) == 1
     @test grid.name == "exponential"
     @test findIndex(0.0042, grid) == 9
     @test def.atom.element.name == "hydrogen"
