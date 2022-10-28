9214f6d7fd9957d988eb4dba5dc4b35e7dcdeea6
= <walra356@planet.nl>
2022-10-28 02:45:10 +0200

    remove fits runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -168,11 +168,11 @@ using Test
     #@test fits_info("T01.fits") == "T01.fits: file was found (for more information set info=true)"
     #@test fits_copy("T01.fits") == "T01.fits was saved as T01 - Copy.fits"
     #@test fits_copy("T01.fits","T01a.fits";protect=false) == "T01.fits was saved as T01a.fits"
-    @test fits_create()
-    @test fits_extend()
-    @test fits_read()
-    @test fits_add_key()
-    @test fits_edit_key()
-    @test fits_delete_key()
-    @test fits_rename_key()
+    #@test fits_create()
+    #@test fits_extend()
+    #@test fits_read()
+    #@test fits_add_key()
+    #@test fits_edit_key()
+    #@test fits_delete_key()
+    #@test fits_rename_key()
 end
