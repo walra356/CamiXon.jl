ea8156c92a21a6916ef71503595a996eb3017fd4
Jook Walraven <69215586+walra356@users.noreply.github.com>
2023-10-16 12:43:58 +0200

    size_threshold = 300000

--- docs/make.jl
+++ docs/make.jl
@@ -9,6 +9,8 @@ makedocs(;
     sitename="CamiXon.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
+        size_threshold_warn = 200000,
+        size_threshold = 300000,
         canonical="https://walra356.github.io/CamiXon.jl",
         assets=String[],
     ),
