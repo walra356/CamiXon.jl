bbb7d5651e37ce526a6cc7d4d8713701138aa46f
Jook Walraven <69215586+walra356@users.noreply.github.com>
2023-10-16 11:12:03 +0200

    work on docs

--- src/dicts.jl
+++ src/dicts.jl
@@ -704,7 +704,47 @@ dictIsotopes = Dict(
     102 => 1100
 
     )
+@doc raw"""
+    dictAntoineCoefficients
 
+Antoine coefficients [A,B,C,D] for temperature ranges below and above the
+melting point. These coefficients are used in the Antoine equation to calculate
+the saturated vapor pressure ``p`` (in Pa) for metalic elements at temperature
+``T`` (in K),
+
+```math
+\mathrm{ln}p=A+B/T+C\mathrm{log_{10}}T+D\cdot T/1000
+```
+
+The quantity
+```math
+L(T) = B +C\cdot T \mathrm{log_{10}}T+D\cdot T^2/1000
+```
+is the latent heat of vaporization (in Joule/K) at temperature ``T``.
+
+Currently, only the coefficients for the metalic elements are implemented -
+see C. B. Alcock, V. P. Itkin and M. K. Horrigan, Canadian Metallurgical
+Quarterly, 23, 309 (1984).
+
+#### Examples:
+```
+julia> dictAntoineCoefficients
+Dict{Int64, Tuple{Any, Any, Tuple{Any, Any, Any}}} with 102 entries:
+  5  => (nothing, nothing, (empty, 2349.0, empty))
+  56 => ([40.0897, -22312.0, -5.27062, 0.0], [20.7526, -18796.0, 0.0, 0.0], (298.0, 1000.0, 1200.0))
+  35 => (nothing, nothing, (empty, 265.8, empty))
+  55 => ([22.3736, -9208.04, 0.0, 0.0], [21.1164, -8818.9, 0.0, 0.0], (298.0, 301.7, 550.0))
+  60 => ([32.2402, -39751.8, -2.19183, 0.0], [22.8364, -36436.1, 0.0, 0.0], (298.0, 1297.0, 2000.0))
+  30 => ([25.5765, -15602.3, 0.0, 0.0], [23.9094, -14474.0, 0.0, 0.0], (298.0, 692.68, 750.0))
+  32 => (nothing, nothing, (empty, 1211.4, empty))
+  ⋮  => ⋮
+```
+To calculate the saturated vapor pressure of Li at T=623 K we use
+```
+julia> svp("Li", 623.0, dictAntoineCoefficients)
+0.0015230367024569058
+```
+"""
 dictAntoineCoefficients = Dict(
     
     1 => (nothing, nothing, (empty, 13.99, empty)),
