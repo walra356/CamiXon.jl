33c1fb9fc12ac2dfc726d81d5b69e7e6c3d9ce68
walra356 <walra356@planet.nl>
2024-11-11 16:38:22 +0100

    work on docs atom.jl

--- src/atom.jl
+++ src/atom.jl
@@ -90,15 +90,14 @@ ionic charge `Q`.
 Output options: `fmt` =  `Object` (default), `String`, `Info`.
 #### Example:
 ```
-listAtom("H", 3, 0) == listAtom(1, 3, 0)
-  true
+julia> listAtom("H", 3, 0) == listAtom(1, 3, 0)
+true
 
-listAtom(1, 3, 0; fmt=Info)
-Element: hydrogen
-    symbol: H
-    element: tritium
-    atomic number: Z = 1
-    atomic weight (relative atomic mass): 1.008
+julia> listAtom(1, 3, 0; fmt=Info)
+Atom: tritium, neutral atom
+  symbol: ³T
+  atomic charge: Z = 1
+  Rydberg charge: Zc = 1
 ```
 """
 function listAtom(Z::Int, A::Int, Q::Int; fmt=Object)
@@ -132,22 +131,22 @@ ionic charge `Q`.
 Output options: `fmt` =  `Object` (default), `String`, `Info`.
 #### Example
 ```
-listAtoms(1,3,0) == listAtoms(1:3,0)
-  true
+julia> listAtoms(1,3,0) == listAtoms(1:3,0)
+true
 
-listAtoms(1:1, 0; fmt=Info);
-  Atom: hydrogen, neutral atom
-    symbol: ¹H
-    atomic charge: Z = 1
-    Rydberg charge: Zc = 1
-  Atom: deuterium, neutral atom
-    symbol: ²D
-    atomic charge: Z = 1
-    Rydberg charge: Zc = 1
-  Atom: tritium, neutral atom
-    symbol: ³T
-    atomic charge: Z = 1
-    Rydberg charge: Zc = 1
+julia> listAtoms(1:1, 0; fmt=Info);
+Atom: hydrogen, neutral atom
+  symbol: ¹H
+  atomic charge: Z = 1
+  Rydberg charge: Zc = 1
+Atom: deuterium, neutral atom
+  symbol: ²D
+  atomic charge: Z = 1
+  Rydberg charge: Zc = 1
+Atom: tritium, neutral atom
+  symbol: ³T
+  atomic charge: Z = 1
+  Rydberg charge: Zc = 1
 ```
 """
 function listAtoms(Z1::Int, Z2::Int, Q::Int; fmt=Object)
@@ -182,26 +181,26 @@ Create Atom with fields:
 * `     .Zc`:  Rydberg charge in a.u. (`::Int`)
 * `.element`:  (`::Element`)
 * `.isotope`:  (`::Isotope`)
+
+`elt`: symbolic element name
 #### Examples:
 ```
-castAtom("Rb"; A=87, Q=0) == castAtom(Z=37, A=87, Q=0)
-  true
+julia> castAtom("Rb"; A=87, Q=0) == castAtom(Z=37, A=87, Q=0)
+true
 
-castAtom(Z=1, A=3, Q=0)
-  Atom(1, 3, 0, 1, Element("hydrogen", "H", 1.008), Isotope("³T", "tritium",
-  1, 3, 2, 1.7591, 3.016049281, 1//2, 1, 12.33, 2.97896246, 0.0, nothing))
+julia> castAtom(Z=1, A=3, Q=0)
+Atom(1, 3, 0, 1, Element("hydrogen", "H", 1.008), Isotope("³T", "tritium", 1, 3, 2, 1.7591, 3.016049281, 1//2, 1, 12.33, 2.97896246, 0.0, nothing))
 
-atom = castAtom(Z=1, A=3, Q=0, msg=true);
-  Element created: H, hydrogen, Z=1, weight=1.008
-  Isotope created: ³T, tritium, Z=1, A=3, N=2, R=1.7591, M=3.016049281, I=1/2⁺, μI=2.97896246, Q=0.0, RA=trace, (radioactive)
-  Atom created: tritium, neutral atom, ³T, Z=1, A=3, Q=0, Zc=1
+julia> atom = castAtom(Z=1, A=3, Q=0, msg=true);
+Element created: H, hydrogen, Z=1, weight=1.008
+Isotope created: ³T, tritium, Z=1, A=3, N=2, R=1.7591, M=3.016049281, I=1/2⁺, μI=2.97896246, Q=0.0, RA=trace, (radioactive)
+Atom created: tritium, neutral atom, ³T, Z=1, A=3, Q=0, Zc=1
 
-atom
-  Atom(1, 3, 0, 1, Element("hydrogen", "H", 1.008), Isotope("³T", "tritium",
-  1, 3, 2, 1.7591, 3.016049281, 1//2, 1, 12.33, 2.97896246, 0.0, nothing))
+julia> atom
+Atom(1, 3, 0, 1, Element("hydrogen", "H", 1.008), Isotope("³T", "tritium", 1, 3, 2, 1.7591, 3.016049281, 1//2, 1, 12.33, 2.97896246, 0.0, nothing))
 
-atom.isotope.T½
-  12.33
+julia> string(atom.isotope.T½) * " seconds"
+"12.33 seconds"
 ```
 """
 function castAtom(;Z=1, A=1, Q=0, msg=false)
