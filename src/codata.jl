0d7286bf10036e57729dd846ee60624e4d5d62ba
walra356 <walra356@planet.nl>
2024-11-11 11:33:07 +0100

    work on Codata coverage

--- src/codata.jl
+++ src/codata.jl
@@ -293,7 +293,7 @@ end
 Method to list the fields of [`Codata`](@ref) by their symbolic name
 #### Example:
 ```
-julia> codata = castCodata(2018);
+julia> julia> codata = castCodata(2018);
 
 julia> listCodata(codata)
 ∆νCs = 9192631770 Hz      - ¹³³Cs hyperfine transition frequency
@@ -305,11 +305,12 @@ julia> listCodata(codata)
   NA = 6.02214e23 mol⁻¹   - Avogadro constant
  Kcd = 683 lm W⁻¹         - Luminous efficacy
   mₑ = 9.10938e-31 kg     - electron mass
-  mₚ = 9.10938e-31 kg     - proton mass
+  mₚ = 1.67262e-27 kg     - proton mass
   R∞ = 1.09737e7 m⁻¹      - Rydberg constant
   Ry = 3.28984e15 Hz      - Rydberg frequency
   Eₕ = 4.35974e-18 J      - Hartree atomic unit
    α = 0.00729735         - fine-structure constant
+  a0 = 5.29177e-11 m      - Bohr radius
   μB = 9.27401e-24 J T⁻¹  - Bohr magneton
   μN = 5.05078e-27 J T⁻¹  - nuclear magneton
   μ₀ = 1.25664e-6 N A⁻²   - magnetic permitivity of vacuum
@@ -318,15 +319,6 @@ julia> listCodata(codata)
   RK = 25812.8 Ω          - Von Klitzing constant
    R = 8.31446 J mol⁻¹K⁻¹ - Molar gas constant
    u = 1.66054e-27 kg     - unified atomic mass unit
-
-julia> codata = castCodata(2022);
-
-julia> strValue.([codata.∆νCs,codata.c,codata.h])
-3-element Vector{String}:
- "9192631770 Hz"
- "299792458 m s⁻¹"
- "6.62607e-34 J Hz⁻¹"
-1.6605390666e-27  
 ```
 """
 function listCodata(codata::Codata; msg=true)
