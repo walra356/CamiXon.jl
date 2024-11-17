6ac3213920c9f1640cea152bcdd37c6563d37d64
walra356 <walra356@planet.nl>
2024-11-17 18:24:07 +0100

    work on docs

--- src/pos.jl
+++ src/pos.jl
@@ -222,17 +222,17 @@ end
     getNcut(f0::T, f::Vector{T}, start::Int, stop::Int) where T<:Real
     getNcut(f0::T, f::Vector{T}, itr::UnitRange) where T<:Real
 
-Index corresponding to the intersection point of the discrete function ``f[n]`` with the value ``f0`` 
-in the interval ``start ≤ n ≤ stop``, 
+Index corresponding to the intersection point of the discrete function ``f[n]`` with the value ``f_0`` 
+in the interval `start ≤ n ≤ stop`, 
 
 ```math
-    f[n_{cut}] = f0.
+    f[n_{cut}] = f_0.
 ```
 
-Condition: ``f[n]`` must be monotonically increasing or decreasing on the interval ``start ≤ n ≤ stop``.
+Condition: ``f[n]`` must be monotonically increasing or decreasing on the interval `start ≤ n ≤ stop`.
 
-NB. For a monotonically *decreasing* function ``n_{cut}`` is approximated by the *largest* ``n`` for which ``f[n] > f0``.
-For a monotonically *increasing* function  ``n_{cut}`` is approximated by the *smallest* ``n`` for which ``f[n] > f0``.
+NB. For a monotonically *decreasing* function ``n_{cut}`` is approximated by the *largest* ``n`` for which ``f[n] > f_0``.
+For a monotonically *increasing* function  ``n_{cut}`` is approximated by the *smallest* ``n`` for which ``f[n] > f_0``.
 """
 function getNcut(f0::T, f::Vector{T}, start::Int, stop::Int) where T<:Real
     
@@ -240,7 +240,7 @@ function getNcut(f0::T, f::Vector{T}, start::Int, stop::Int) where T<:Real
      m = stop    
     Δn = (m-n)÷2
 
-    raising = (f[n] ≤ f0 ≤ f[m]) ? true : (f[n] ≥ f0 ≥ f[m]) ? false : error("Error: value outside function range")
+    raising = (f[n] ≤ f0 ≤ f[m]) ? true : (f[n] ≥ f0 ≥ f[m]) ? false : throw(DomainError(f0, "value outside function range"))
 
     while Δn ≥ 1
         if raising
