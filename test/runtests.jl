edfcb8f0b069e515cca6e55f0a45fcb754f19198
walra356 <walra356@planet.nl>
2025-01-16 12:53:24 +0100

    work on runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -115,7 +115,7 @@ using Test
     def = castDef(grid, atom, orbit, codata);
     def, adams, init, Z = adams_moulton_nodes(E, scr, grid, def; imax=25, msg=false);
     def, adams, init, Z = adams_moulton_iterate!(Z, init, grid, def, adams; imax=25, ϵ=1e-15, msg=false);
- #   @test ZH2p_generic ≈ Z
+    @test ZH2p_generic ≈ Z
 #   ---------------------------------------------------------------------------------------- 
     atom = castAtom(Z=1, A=1, Q=0; msg=false);
     orbit = castOrbit(n=1, ℓ=0; msg=false);
@@ -132,14 +132,14 @@ using Test
     def, adams, init, Z = adams_moulton_nodes(E, scr, grid, def; imax=25, msg=false);
     def, adams, init, Z = adams_moulton_iterate!(Z, init, grid, def, adams; imax=5, ϵ=1e-15, msg=false);
     def, adams, init, Z = adams_moulton_iterate!(Z, init, grid, def, adams; imax=25, ϵ=1e-15, msg=false);
- #   @test ZH1s_generic ≈ Z
+    @test ZH1s_generic ≈ Z
     @test grid.name == "exponential"
     @test findIndex(0.0042, grid) == 220
     @test def.atom.element.name == "hydrogen"
     @test CamiDiff.grid_integration(real(ZH1s_generic) .^ 2, grid, 1, grid.N) ≈ 1.0
-    @test round(Int, UF(0, real(Z), grid)[1]) == 1
+ #   @test round(Int, UF(0, real(Z), grid)[1]) == 1
     grid, def, adams, init, Z = adams_moulton_precise!(Z, init, grid, def; imax=5, ϵ=1e-20, msg=false);
- #   @test ZH1s_generic ≈ Z
+    @test ZH1s_generic ≈ Z
 #   ---------------------------------------------------------------------------------------- 
     #Z1 = hydrogenic_reduced_wavefunction(1, orbit, grid);
     #P = real(Z)
