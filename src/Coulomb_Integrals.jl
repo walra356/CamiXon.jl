2c79da5c43203d15cfa7306a41ca569b95d7a258
walra356 <walra356@planet.nl>
2025-02-28 10:35:19 +0100

    work on docs

--- src/Coulomb_Integrals.jl
+++ src/Coulomb_Integrals.jl
@@ -286,7 +286,7 @@ function UG(orbit1::Orbit, orbit2::Orbit, P1::Vector{T}, P2::Vector{T}, grid::Ca
 end
 
 # ------------------------------------------------------------------------------
-#                       Fk(k, P, grid)
+#                       Fk(k, P1, P2, grid)
 # ------------------------------------------------------------------------------
 
 @doc raw"""   
@@ -294,12 +294,11 @@ end
 
 ``k^{th}``-order contribution to the *direct* radial integral over the (reduced) 
 radial wavefunctions `P1` and `P2` of two electrons (in the orbitals 
-``nl`` and ``n^{\prime}l^{\prime}`` in a central potential field.
+``nl`` and ``n^{\prime}l^{\prime}``) in a central potential field.
 
 ```math
 F^{k}(nl;n^{\prime}l^{\prime})
-=\int_{0}^{\infty}U_{F}^{k}(nl;\rho)\left[\tilde{R}_{n^{\prime}l^{\prime}}(\rho)\right]^{2}\rho^{2}d\rho
-=\int_{0}^{\infty}U_{F}^{k}(n^{\prime}l^{\prime};\rho)\left[\tilde{R}_{nl}}(\rho)\right]^{2}\rho^{2}d\rho.
+=\int_{0}^{\infty}U_{F}^{k}(nl;\rho)\left[\tilde{R}_{n^{\prime}l^{\prime}}(\rho)\right]^{2}\rho^{2}d\rho.
 ```
 
     Fk(k::Int, P::Vector{T}, grid::CamiDiff.Grid) where T<:Real
@@ -308,7 +307,7 @@ F^{k}(nl;n^{\prime}l^{\prime})
 radial wavefunction `P` of two *equivalent* ``nl`` electrons in a central potential.
 
 ```math
-F^{k}(nl;n^{\prime}l^{\prime})
+F^{k}(nl)
 =\int_{0}^{\infty}U_{F}^{k}(nl;\rho)\left[\tilde{R}_{nl}(\rho)\right]^{2}\rho^{2}d\rho.
 ```
 """
