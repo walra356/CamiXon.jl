93873fce67978d48583087ca0af03af71c5f1f02
walra356 <walra356@planet.nl>
2025-03-14 09:48:00 +0100

    work on docs

--- src/orbit.jl
+++ src/orbit.jl
@@ -55,7 +55,7 @@ Create `Orbit` with fields:
 * `.n′`:  radial quantum number (number of nodes in radial wavefunction)
 * ` .ℓ`:  orbital angular momentum valence electron
 * `.mℓ`:  orbital angular momentum projection valence electron
-#### Examples:
+#### Example:
 ```
 julia> castOrbit(n=1, ℓ=0; msg=true)
 Orbital: 1s
@@ -64,6 +64,18 @@ Orbital: 1s
     orbital angular momentum of valence electron: ℓ = 0
     orbital angular momentum projection of valence electron: mℓ = 0
 Orbit("1s", 1, 0, 0, 0)
+```
+    castOrbit(strOrbit::String; mℓ=0, msg=false)
+
+#### Example:
+```
+julia> castOrbit1("2p"; mℓ=-1, msg=true)
+Orbital: 2p
+    principal quantum number: n = 2
+    radial quantum number: n′ = 0 (number of nodes in radial wavefunction)
+    orbital angular momentum of valence electron: ℓ = 1
+    orbital angular momentum projection of valence electron: mℓ = -1
+Orbit("2p", 2, 0, 1, -1)
 ```
 """
 function castOrbit(;n=1, ℓ=0, mℓ=0, msg=false)
@@ -81,6 +93,15 @@ function castOrbit(;n=1, ℓ=0, mℓ=0, msg=false)
 
     return Orbit(name, n, n′, ℓ, mℓ)
 
+end
+function castOrbit(strOrbit::String; mℓ=0, msg=false)
+
+    nl = strip(lowercase(strOrbit))
+
+    n, ℓ = get(dictAtomicOrbitals, nl, nothing)
+
+    return castOrbit(;n, ℓ, mℓ, msg)
+        
 end
 
 # ========== Spinorbit(name::String, orbit::Orbit, ms::Rational(Int)) ===========
@@ -134,6 +155,18 @@ Spinorbital: 1s↑
     spin magnetic quantum number: ms = 1/2
 Spinorbit("1s↑", Orbit("1s", 1, 0, 0, 0), 1//2)
 ```
+    castSpinorbit(strOrbit::String; mℓ=0, ms=1/2, msg=false)
+
+#### Example:
+```
+julia> Spinorbital: 2p↓
+    principal quantum number: n = 2
+    radial quantum number: n′ = 0 (number of nodes in radial wavefunction)
+    orbital angular momentum of valence electron: ℓ = 1
+    orbital angular momentum projection of valence electron: mℓ = 1
+    spin magnetic quantum number: ms = -1/2
+Spinorbit("2p↓", Orbit("2p", 2, 0, 1, 1), -1//2)
+```
 """
 function castSpinorbit(;n=1, ℓ=0, mℓ=0, ms=1/2, msg=false)
 
@@ -149,6 +182,15 @@ function castSpinorbit(;n=1, ℓ=0, mℓ=0, ms=1/2, msg=false)
 
     return Spinorbit(name, o, ms)
 
+end
+function castSpinorbit(strOrbit::String; mℓ=0, ms=1/2, msg=false)
+
+    nl = strip(lowercase(strOrbit))
+
+    n, ℓ = get(dictAtomicOrbitals, nl, nothing)
+
+    return castSpinorbit(;n, ℓ, mℓ, ms, msg)
+        
 end
 
 # ------------------------------------------------------------------------------
@@ -239,6 +281,7 @@ end
 # ------------------------------------------------------------------------------
 #                       Shells(name, N, n, ℓ)
 # ------------------------------------------------------------------------------
+
 @doc raw"""
     Shells(name, shells)
 
