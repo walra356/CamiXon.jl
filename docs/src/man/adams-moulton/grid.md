8995dfd3d8e99cbb97f86d67e72220b245c45e46
walra356 <walra356@planet.nl>
2025-03-20 20:33:46 +0100

    work on docs

--- docs/src/man/adams-moulton/grid.md
+++ docs/src/man/adams-moulton/grid.md
@@ -6,6 +6,7 @@ functions of `N` elements representing the grid function and its derivative.
 
 #### autoGrid
 ```@docs
+autoGrid(atom::Atom, orbit::Orbit, T::Type; p=0, rmax=0, N=0, polynom=[], epn=5, k=5, msg=false)
 autoGrid(atom::Atom, spinorbit::Spinorbit, T::Type; p=0, rmax=0, N=0, polynom=[], epn=5, k=5, msg=false)
 ```
 
--- src/grid_autoset.jl
+++ src/grid_autoset.jl
@@ -114,18 +114,7 @@ julia> orbit = castOrbit(n=75, ℓ=0, msg=false);
 
 julia> grid = autoGrid(atom, orbit, Float64; msg=true);
 Grid: exponential, Float64, rmax = 14137.5, N = 7900, h = 0.00126582, r0 = 0.642684
-```
-    autoGrid(atom::Atom, spinorbit::Spinorbit, T::Type; p=0, rmax=0, N=0, polynom=[], epn=5, k=5, msg=false)
 
-```
-atom = castAtom(;Z=1, A=1, Q=0, msg=false);
-
-julia> spinorbit = castSpinorbit(n=75, ℓ=0, msg=false);
-
-julia> grid = autoGrid(atom, spinorbit, Float64; msg=true);
-Grid: exponential, Float64, rmax = 14137.5, N = 7900, h = 0.00126582, r0 = 0.642684
-```
-```
 plot_gridfunction(grid, 1:grid.N; title="")
 ```
 The plot is made using CairomMakie.
@@ -151,6 +140,36 @@ function autoGrid(atom::Atom, orbit::Orbit, T::Type; p=0, rmax=0, N=0, polynom=[
     return CamiDiff.castGrid(ID, N, T; h, rmax, p, polynom, epn, k, msg)
 
 end
+
+
+
+@doc raw"""
+    autoGrid(atom::Atom, spinorbit::Spinorbit, T::Type; p=0, rmax=0, N=0, polynom=[], epn=5, k=5, msg=false)
+
+Automatic setting of grid parameters for a given orbit [`Orbit`](@ref) or an
+array of orbits - `orbits = [orbit1, orbit2, ⋯]`. Important cases:
+* `p == 0` (exponential radial grid)
+* `p == 1` (linear radial grid)
+* `p > 1` (quasi-exponential radial grid)
+* `polynom=[]` (free polynomial grid based on the `polynom`)
+* `Nboost` (multiplier to boost numerical precision)
+* `epn` (endpoint number: odd number to be used for trapezoidal integration with endpoint correction)
+* `k` (Adams-Moulton order to be used for `k+1`-point Adams-Moulton integration)
+#### Example:
+```
+atom = castAtom(;Z=1, A=1, Q=0, msg=false);
+
+julia> spinorbit = castSpinorbit(n=75, ℓ=0, msg=false);
+
+julia> grid = autoGrid(atom, spinorbit, Float64; msg=true);
+Grid: exponential, Float64, rmax = 14137.5, N = 7900, h = 0.00126582, r0 = 0.642684
+
+plot_gridfunction(grid, 1:grid.N; title="")
+```
+The plot is made using CairomMakie.
+NB.: `plot_gridfunction` is not part of the `CamiXon` package.
+![Image](../../assets/exponential_grid.png)
+"""
 function autoGrid(atom::Atom, spinorbit::Spinorbit, T::Type; p=0, rmax=0, N=0, polynom=[], epn=5, k=5, msg=false)
 
     T ∈ [Float64,BigFloat] || println("autoGrid: grid.T = $T => Float64 (was enforced by automatic type promotion)")
